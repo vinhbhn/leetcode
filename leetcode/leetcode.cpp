8e281763e4cbc482f8b399d4e428dd56944fc674
vinhbhn <bvinhhn@gmail.com>
2023-09-15 22:43:28 +0700

    easy: sqrtx

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -3,90 +3,75 @@
 #include <string>
 #include <algorithm>
 
-// because it's already sort 
-// index :space between number duplicate
-// after all: then add 1 to index because func return number (not duplicate), not space
-//int removeDuplicates(std::vector<int>& nums) {
-//	int k{ 0 };
-//	for (int i{ 0 }; i < nums.size() - 1 ; ++i)
-//	{
-//		if (nums[i] != nums[i + 1])
-//		{
-//			++k;
-//		}
-//	}
-// 
-// 	for (auto num : nums)
-//		std::cout << num << ' ';
-//	std::cout << '\n';
+//int mySqrt(int x) {
+//	if (x == 1 || x == 0)
+//		return x;
 //
-//	return ++k;
-//}
-
-// find min, max -> vector<int> [min, max]
-// k = max - min + 1
-//int removeDuplicates(std::vector<int>& nums) {
-//	auto [min, max] = std::minmax_element(std::begin(nums), std::end(nums));
-//	int value{ *min };
-//
-//	for (int i{ 0 }; i <= nums.size(); ++i)
+//	int i{ 2 };
+//	unsigned long long cal{ 0 };
+//	while (i <= x)
 //	{
-//		nums[i] = value++;
-//		if (value > *max)
-//			break;
+//		cal = i * i;
+//		if (cal == x)
+//			return i;
+//
+//		if (cal > x)
+//			return i - 1;
+//
+//		++i;
 //	}
 //
-//	for (auto num : nums)
-//		std::cout << num << ' ';
-//	std::cout << '\n';
-//
-//	return (*max - *min + 1);
+//	return i;
 //}
 
-//int removeDuplicates(std::vector<int>& nums) {
-//	int k{ 0 };
-//	for (std::vector<int>::iterator it = nums.begin(); it != nums.end();)
-//	{
-//		if (*it == *(it + 1) && (it+1) != nums.end())
-//		{
-//			it = nums.erase(it);
-//		}
-//		else
-//			++k;
-//	}
-//
-//	for (auto num : nums)
-//		std::cout << num << ' ';
-//	std::cout << '\n';
-//
-//	return ++k;
-//}
+int mySqrt(int x) {
+    if (x == 1 || x == 0)
+        return x;
 
-// https://leetcode.com/problems/remove-duplicates-from-sorted-array/solutions/3676877/best-method-100-c-java-python-beginner-friendly/
-int removeDuplicates(std::vector<int>& nums) {
-	int index{ 1 };
-	for (int i{ 1 }; i < nums.size(); ++i)
-	{
-		if (nums[i] != nums[i - 1])
-		{
-			nums[index] = nums[i];
-			++index;
-		}
-	}
+    int i{ 2 };
+    while (i <= x)
+    {
+        if (x / i == i)
+            return i;
+        else if (x / i < i)
+            return i - 1;
 
-	return index;
+        ++i;
+    }
 
+    return i;
 }
 
+// https://leetcode.com/problems/sqrtx/solutions/
+int mySqrt(int x) {
+    if (x == 0)
+        return x;
+    int first = 1, last = x;
+    int mid{ -1 };
+    while (first <= last) {
+        mid = first + (last - first) / 2;
+        // mid * mid == x gives runtime error
+        if (mid == x / mid)
+            return mid;
+        else if (mid > x / mid) {
+            last = mid - 1;
+        }
+        else {
+            first = mid + 1;
+        }
+    }
+    return last;
+}
 
 int main()
 {
-	std::vector<int> nums1{ 0,0,1,1,1,2,2,3,3,4 };
-	std::cout << removeDuplicates(nums1) << '\n';
-	std::vector<int> nums2{ 1,1,2 };
-	std::cout << removeDuplicates(nums2) << '\n';
-	std::vector<int> nums3{ 0, 0, 0, 0, 3 };
-	std::cout << removeDuplicates(nums3) << '\n';
+	std::cout << mySqrt(2) << '\n';
+	std::cout << mySqrt(4) << '\n';
+	std::cout << mySqrt(8) << '\n';
+	std::cout << mySqrt(16) << '\n';
+	std::cout << mySqrt(46431) << '\n';
+	std::cout << mySqrt(2147483647) << '\n';
+
 
 
 	return 0;

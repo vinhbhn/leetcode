b4fabe261ae031e0573fb831e1d74099e3dc6156
vinhbhn <bvinhhn@gmail.com>
2023-09-15 22:02:34 +0700

    easy: remove duplicate (answer)

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -3,48 +3,91 @@
 #include <string>
 #include <algorithm>
 
-int removeElement(std::vector<int>& nums, int val) {
-	int numsOfVal{};
-
-	// https://en.cppreference.com/w/cpp/container/vector/erase
-	for (std::vector<int>::iterator it = nums.begin(); it != nums.end();)
-	{
-		if (*it == val)
-		{
-			++numsOfVal;
-			it = nums.erase(it);
-		}
-		else
-			++it;
-	}
-
-	for (int i{ 0 }; i < numsOfVal; ++i)
-		nums.push_back(val);
-
-	return (nums.size() - numsOfVal);
-}
-
-// https://leetcode.com/problems/remove-element/solutions/3670940/best-100-c-java-python-beginner-friendly/
-//int removeElement(std::vector<int>& nums, int val) {
-//	int index{ 0 };
-//	for (int i{ 0 }; i < nums.size(); ++i)
+// because it's already sort 
+// index :space between number duplicate
+// after all: then add 1 to index because func return number (not duplicate), not space
+//int removeDuplicates(std::vector<int>& nums) {
+//	int k{ 0 };
+//	for (int i{ 0 }; i < nums.size() - 1 ; ++i)
 //	{
-//		if (nums[i] != val)
+//		if (nums[i] != nums[i + 1])
 //		{
-//			nums[index] = nums[i];
-//			++index;
+//			++k;
 //		}
 //	}
-//	return index;
+// 
+// 	for (auto num : nums)
+//		std::cout << num << ' ';
+//	std::cout << '\n';
+//
+//	return ++k;
 //}
+
+// find min, max -> vector<int> [min, max]
+// k = max - min + 1
+//int removeDuplicates(std::vector<int>& nums) {
+//	auto [min, max] = std::minmax_element(std::begin(nums), std::end(nums));
+//	int value{ *min };
+//
+//	for (int i{ 0 }; i <= nums.size(); ++i)
+//	{
+//		nums[i] = value++;
+//		if (value > *max)
+//			break;
+//	}
+//
+//	for (auto num : nums)
+//		std::cout << num << ' ';
+//	std::cout << '\n';
+//
+//	return (*max - *min + 1);
+//}
+
+//int removeDuplicates(std::vector<int>& nums) {
+//	int k{ 0 };
+//	for (std::vector<int>::iterator it = nums.begin(); it != nums.end();)
+//	{
+//		if (*it == *(it + 1) && (it+1) != nums.end())
+//		{
+//			it = nums.erase(it);
+//		}
+//		else
+//			++k;
+//	}
+//
+//	for (auto num : nums)
+//		std::cout << num << ' ';
+//	std::cout << '\n';
+//
+//	return ++k;
+//}
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/solutions/3676877/best-method-100-c-java-python-beginner-friendly/
+int removeDuplicates(std::vector<int>& nums) {
+	int index{ 1 };
+	for (int i{ 1 }; i < nums.size(); ++i)
+	{
+		if (nums[i] != nums[i - 1])
+		{
+			nums[index] = nums[i];
+			++index;
+		}
+	}
+
+	return index;
+
+}
 
 
 int main()
 {
-	std::vector<int> nums1{ 0,1,2,2,3,0,4,2 };
-	std::cout << removeElement(nums1, 2) << '\n';
-	std::vector<int> nums2{ 2,2,3,3 };
-	std::cout << removeElement(nums2, 3) << '\n';
+	std::vector<int> nums1{ 0,0,1,1,1,2,2,3,3,4 };
+	std::cout << removeDuplicates(nums1) << '\n';
+	std::vector<int> nums2{ 1,1,2 };
+	std::cout << removeDuplicates(nums2) << '\n';
+	std::vector<int> nums3{ 0, 0, 0, 0, 3 };
+	std::cout << removeDuplicates(nums3) << '\n';
+
 
 	return 0;
 }

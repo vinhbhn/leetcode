c23088b0eb71e98c64dab838ede46091b9836164
vinhbhn <bvinhhn@gmail.com>
2023-09-15 20:36:05 +0700

    easy: remove element

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -3,30 +3,48 @@
 #include <string>
 #include <algorithm>
 
-std::string longestCommonPrefix(std::vector<std::string>& strs)
-{
-	std::sort(strs.begin(), strs.end()); // sorting the array
-	std::string ans{};
-	for (int i{ 0 }; i < strs[0].size(); ++i) // max length = element 1 length of strs
-	{
-		if (strs[0][i] != strs[strs.size() - 1][i])
-			break;
+int removeElement(std::vector<int>& nums, int val) {
+	int numsOfVal{};
 
-		ans += strs[0][i]; // concatinate if the characters are matching
+	// https://en.cppreference.com/w/cpp/container/vector/erase
+	for (std::vector<int>::iterator it = nums.begin(); it != nums.end();)
+	{
+		if (*it == val)
+		{
+			++numsOfVal;
+			it = nums.erase(it);
+		}
+		else
+			++it;
 	}
 
-	return ans;
+	for (int i{ 0 }; i < numsOfVal; ++i)
+		nums.push_back(val);
+
+	return (nums.size() - numsOfVal);
 }
+
+// https://leetcode.com/problems/remove-element/solutions/3670940/best-100-c-java-python-beginner-friendly/
+//int removeElement(std::vector<int>& nums, int val) {
+//	int index{ 0 };
+//	for (int i{ 0 }; i < nums.size(); ++i)
+//	{
+//		if (nums[i] != val)
+//		{
+//			nums[index] = nums[i];
+//			++index;
+//		}
+//	}
+//	return index;
+//}
 
 
 int main()
 {
-	std::vector<std::string> strs1 = { "flower", "flow", "flight" };
-	std::cout << longestCommonPrefix(strs1) << '\n';
-	std::vector<std::string> strs2 = { "dog", "racecar", "car" };
-	std::cout << longestCommonPrefix(strs2) << '\n';
-	std::vector<std::string> strs3 = { "a" };
-	std::cout << longestCommonPrefix(strs3) << '\n';
+	std::vector<int> nums1{ 0,1,2,2,3,0,4,2 };
+	std::cout << removeElement(nums1, 2) << '\n';
+	std::vector<int> nums2{ 2,2,3,3 };
+	std::cout << removeElement(nums2, 3) << '\n';
 
 	return 0;
 }

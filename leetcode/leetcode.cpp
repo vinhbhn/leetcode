c6435a529379ee4a079020eb399dac1cd5f79628
vinhbhn <bvinhhn@gmail.com>
2023-09-19 16:20:55 +0700

    easy: search insert position

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -3,30 +3,84 @@
 #include <string>
 #include <algorithm>
 #include <functional>
+#include <stack>
 
-int reverse(int x)
+//int searchInsert(std::vector<int>& nums, int target) {
+//	if (static_cast<int>(nums.size()) == 1)
+//	{
+//		if (target > nums[0])
+//			return 1;
+//		else
+//			return 0;
+//	}
+//
+//	int current{}, last{ static_cast<int>(nums.size()) - 1 };
+//
+//	for (int first{ 0 }; first <= last; )
+//	{
+//		if (first + 1 == last)
+//		{
+//			if (target <= nums[first])
+//				return first;
+//			else if (target > nums[last])
+//				return last + 1;
+//			else
+//				return last;
+//
+//		}
+//
+//		int current = (first + last) / 2;
+//
+//		if (target == nums[current])
+//			return current;
+//		else if (target > nums[current])
+//		{
+//			first = current;
+//		}
+//		else
+//		{
+//			last = current;
+//		}
+//	}
+//
+//	return current;
+//}
+
+int searchInsert(std::vector<int>& nums, int target)
 {
-	long number{ 0 };
-	while (x)
+	int first{ 0 }, last{ static_cast<int>(nums.size()) }, mid{};
+
+	if (target > nums[last - 1])
+		return last;
+
+	while (first <= last)
 	{
-		number = number * 10 + x % 10;
-		x /= 10;
+		mid = (first + last) / 2;
+		if (target == nums[mid])
+			return mid;
+		else if (target > nums[mid])
+			first = mid + 1;
+		else
+			last = mid - 1;
 	}
 
-	if (number > INT_MAX || number < INT_MIN)
-		return 0;
-
-	return number;
+	return first;
 }
-
 
 int main()
 {
-	std::cout << reverse(123) << '\n';
-	std::cout << reverse(-123) << '\n';
-	std::cout << reverse(120) << '\n';
-	std::cout << reverse(12345) << '\n';
-	std::cout << reverse(1534236469) << '\n'; // return 0
+	std::vector<int> s{ 1,3,5,6 };
+	int target = 5;
+	std::cout << searchInsert(s, target) << '\n';
+	target = 2;
+	std::cout << searchInsert(s, target) << '\n';
+	target = 7;
+	std::cout << searchInsert(s, target) << '\n';
+	target = 0;
+	std::cout << searchInsert(s, target) << '\n';
+	s = { 1 };
+	std::cout << searchInsert(s, target) << '\n';
+
 
 	return 0;
 }

949aaf23a82c4aeedb4237b3977449be350cdb7c
vinhbhn <bvinhhn@gmail.com>
2023-09-20 21:56:44 +0700

    easy: a number after a double reversal

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -8,24 +8,37 @@
 #include <bitset>
 #include <cstdint>
 
-std::uint32_t reverseBits(std::uint32_t n) {
-	int ans{ 0 };
-	for (int i{ 0 }; i < 32; i++)
+bool isSameAfterReversals(int num) {
+	long number{ num }, reversed1{ 0 };
+	while (number)
 	{
-		ans <<= 1;
-		if (n & 1)
-			ans ^= 1;
-
-		n >>= 1;
+		reversed1 = reversed1 * 10 + number % 10;
+		number /= 10;
 	}
-	return ans;
+
+	long number2{ reversed1 }, reversed2{ 0 };
+	while (number2)
+	{
+		reversed2 = reversed2 * 10 + number2 % 10;
+		number2 /= 10;
+	}
+
+	return (num == reversed2);
+}
+
+bool isSameAfterReversals(int num) {
+	if (num == 0) return true;
+	if (num % 10 == 0) return false;
+	return true;
 }
 
 
 int main()
 {
-	std::cout << reverseBits(0b00000010100101000001111010011100) << '\n';
-	std::cout << reverseBits(0b11111111111111111111111111111101) << '\n';
+	std::cout << std::boolalpha;
+	std::cout << isSameAfterReversals(526) << '\n';
+	std::cout << isSameAfterReversals(1800) << '\n';
+	std::cout << isSameAfterReversals(0) << '\n';
 
 
 	return 0;

fdf0a7da3b16f3e1dc5904a185d73909168b27ef
vinhbhn <bvinhhn@gmail.com>
2023-09-23 17:45:00 +0700

    easy: contains duplicate answer

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -7,30 +7,58 @@
 #include <bit>
 #include <bitset>
 #include <cstdint>
+#include <unordered_set>
 
-bool containsDuplicate(std::vector<int>& nums) {
-	std::sort(nums.begin(), nums.end());
+// O(n logn)
+//bool containsDuplicate(std::vector<int>& nums) {
+//	std::sort(nums.begin(), nums.end());
+//
+//	for (int i{ 0 }; i < static_cast<int>(nums.size()) - 1; i++)
+//		if (nums[i] == nums[i + 1])
+//			return true;
+//
+//	return false;
+//}
+//
+//bool containsDuplicate(std::vector<int>& nums) {
+//	std::sort(nums.begin(), nums.end());
+//
+//	auto it = std::adjacent_find(nums.begin(), nums.end());
+//	if (it == nums.end())
+//		return false;
+//	else
+//		return true;
+//
+//	return false;
+//}
 
-	for (int i{ 0 }; i < static_cast<int>(nums.size()) - 1; i++)
-		if (nums[i] == nums[i + 1])
+//https://leetcode.com/problems/contains-duplicate/solutions/3672475/4-method-s-c-java-python-beginner-friendly/
+// hash set
+// O(n)
+//bool containsDuplicate(std::vector<int>& nums) {
+//	std::unordered_set<int> seen;
+//	for (int num : nums)
+//	{
+//		if (seen.count(num) > 0)
+//			return true;
+//		seen.insert(num);
+//	}
+//	return false;
+//}
+
+// hash map
+// O(n)
+bool containsDuplicate(std::vector<int>& nums)
+{
+	std::unordered_map<int, int> seen;
+	for (int num : nums)
+	{
+		if (seen[num] >= 1)
 			return true;
-
+		seen[num]++;
+	}
 	return false;
 }
-
-bool containsDuplicate(std::vector<int>& nums) {
-	std::sort(nums.begin(), nums.end());
-
-	auto it = std::adjacent_find(nums.begin(), nums.end());
-	if (it == nums.end())
-		return false;
-	else
-		return true;
-
-	return false;
-}
-
-
 
 int main()
 {

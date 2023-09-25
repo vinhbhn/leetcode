72882d0f576c31887b18230919241b0fb4633139
vinhbhn <bvinhhn@gmail.com>
2023-09-25 17:55:03 +0700

    easy: power of two

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -9,65 +9,45 @@
 #include <cstdint>
 #include <unordered_set>
 
-// too slow
-//bool isPowerOfThree(int n) {
+//bool isPowerOfTwo(int n) {
 //	if (n <= 0)
 //		return false;
 //
-//	for (int i = 0; i < std::sqrt(n); i++)
+//	while (n > 1)
 //	{
-//		if (std::pow(3, i) == n)
-//			return true;
-//	}
-//
-//	return false;
-//}
-
-//bool isPowerOfThree(int n) {
-//	if (n <= 0)
-//		return false;
-//
-//	while (n > 1) {
-//		if (n % 3 != 0)
+//		if (n % 2 != 0)
 //			return false;
-//
-//		n /= 3;
+//		n /= 2;
 //	}
 //
 //	return true;
 //}
 
-//https://leetcode.com/problems/power-of-three/solutions/3230079/best-c-3-solution-math-recursion-one-stop-solution/
-// using math O(1) O(1)
-//bool isPowerOfThree(int n) {
-//	return n > 0 && 1162261467 % n == 0;
+// https://leetcode.com/problems/power-of-two/solutions/1638704/c-easy-to-solve-all-interview-approaches-with-detailed-explanations/
+//Constraints[-2 ^ 31 <= n <= 2 ^ 31 - 1] also 2 ^ 30 is 1073741824 (using math)
+//bool isPowerOfTwo(int n) {
+//	return (n > 0 && 1073741824 % n == 0);
 //}
-// recursive O(logN) O(logN)
-bool isPowerOfThree(int n) {
-	if (n == 0)
-		return false;
-	else if (n == 1)
-		return true;
-
-	return (n % 3 == 0) && isPowerOfThree(n / 3);
+// using ceil-floor method O(logN) O(1)
+//bool isPowerOfTwo(int n) {
+//	if (n <= 0)
+//		return false;
+//
+//	return std::ceil(log2(n)) == std::floor(log2(n));
+//}
+// bit manipulation O(1) O(1)
+//bool isPowerOfTwo(int n) {
+//	if (n <= 0) return false;
+//	return ((n & (n - 1)) == 0);
+//}
+// using recursive O(logN) O(logN)
+bool isPowerOfTwo(int n) {
+	if (n <= 0) return false;
+	return ((n == 1) || ((n % 2 == 0) && isPowerOfTwo(n / 2)));
 }
-
-
 
 int main()
 {
-	int n = 27;
-	std::cout << std::boolalpha;
-	std::cout << isPowerOfThree(n) << '\n';
-
-	n = 0;
-	std::cout << isPowerOfThree(n) << '\n';
-
-	n = -1;
-	std::cout << isPowerOfThree(n) << '\n';
-
-	std::cout << isPowerOfThree(1) << '\n';
-
 
 	return 0;
 }

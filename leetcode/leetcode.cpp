09d7f23b19f90b09ba6b02dafcd3cdd2cea850b5
vinhbhn <bvinhhn@gmail.com>
2023-09-25 15:28:44 +0700

    easy: single number II

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -11,43 +11,56 @@
 
 // hash map
 // O(n) O(n)
+//int singleNumber(std::vector<int>& nums) {
+//	std::unordered_map<int, int> s;
+//
+//	for (auto num : nums)
+//		s[num]++;
+//
+//	for (auto x : s)
+//		if (x.second == 1)
+//			return x.first;
+//
+//	return 0;
+//}
+
+// https://leetcode.com/problems/single-number-ii/solutions/3714928/bit-manipulation-c-java-python-beginner-friendly/
+// bit manipulation
+//int singleNumber(std::vector<int>& nums) {
+//	int ans{ 0 };
+//
+//	for (int i{ 0 }; i < 32; i++)
+//	{
+//		int sum = 0;
+//		for (const int num : nums)
+//			sum += num >> i & 1;
+//		sum %= 3;
+//		ans |= sum << i;
+//	}
+//
+//	return ans;
+//}
+// magic
 int singleNumber(std::vector<int>& nums) {
-	std::unordered_map<int, int> s;
+	int ones{ 0 }, twos{ 0 };
 
-	for (auto num : nums)
-		s[num]++;
+	for (const int num : nums)
+	{
+		ones ^= (num & ~twos);
+		twos ^= (num & ~ones);
+	}
 
-	for (auto x : s)
-		if (x.second == 1)
-			return x.first;
-
-	return 0;
+	return ones;
 }
-
-// constant space
-// O(n) O(1)
-int singleNumber(std::vector<int>& nums) {
-	int ans{ 0 };
-
-	for (auto num : nums)
-		ans ^= num;
-
-	return ans;
-}
-
 
 
 int main()
 {
-	std::vector<int> nums = { 2,2,1 };
+	std::vector<int> nums = { 2,2,3,2 };
 	std::cout << singleNumber(nums) << '\n';
 
-	nums = { 4,1,2,1,2 };
+	nums = { 0,1,0,1,0,1,99 };
 	std::cout << singleNumber(nums) << '\n';
-
-	nums = { 1 };
-	std::cout << singleNumber(nums) << '\n';
-
 
 	return 0;
 }

875ae3b8e26f11fd1329265dc1d0455c97bb8142
vinhbhn <bvinhhn@gmail.com>
2023-09-26 14:43:43 +0700

    medium: find the duplicate number

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -9,20 +9,50 @@
 #include <cstdint>
 #include <unordered_set>
 
-// https://leetcode.com/problems/find-the-difference/solutions/4086631/99-78-xor-sum/?envType=daily-question&envId=2023-09-25
-// use XOR: O(n) O(1)
-char findTheDifference(std::string s, std::string t) {
-	char result{};
-	for (char c : s + t)
-		result ^= c;
+// O(n) O(n)
+//int findDuplicate(std::vector<int>& nums) {
+//	std::unordered_map<int, int> mp;
+//
+//	for (auto num : nums)
+//		mp[num]++;
+//
+//	for (auto x : mp)
+//		if (x.second >= 2)
+//			return x.first;
+//
+//	return {};
+//}
 
-	return result;
+// https://leetcode.com/problems/find-the-duplicate-number/solutions/4062141/97-77-6-approaches-set-count-binary-search-fast-slow-mark-sort/
+// fast-slow pointer approach (Floyd's cycle detection) O(n) O(1)
+int findDuplicate(std::vector<int>& nums) {
+	int slow = nums[0], fast = nums[0];
+
+	do {
+		slow = nums[slow];
+		fast = nums[nums[fast]];
+	} while (slow != fast);
+
+	slow = nums[0];
+	while (slow != fast)
+	{
+		slow = nums[slow];
+		fast = nums[fast];
+	}
+
+	return slow;
 }
 
 int main()
 {
-	std::cout << findTheDifference("abcd", "abcde") << '\n';
-	std::cout << findTheDifference("", "y") << '\n';
+	std::vector nums = { 1,3,4,2,2 };
+	std::cout << findDuplicate(nums) << '\n';
+
+	nums = { 3,1,3,4,2 };
+	std::cout << findDuplicate(nums) << '\n';
+
+	nums = { 2, 2, 2, 2, 2 };
+	std::cout << findDuplicate(nums) << '\n';
 
 	return 0;
 }

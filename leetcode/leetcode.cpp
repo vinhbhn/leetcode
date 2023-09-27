0dd26a20d68b5455790a1e33e79e5c389e61d8fc
vinhbhn <bvinhhn@gmail.com>
2023-09-27 21:53:14 +0700

    easy: 2351. first letter to appear twice

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -10,73 +10,31 @@
 #include <unordered_set>
 #include <set>
 
-//int firstUniqChar(std::string s) {
-//	std::unordered_map<char, int> mp;
-//	int pos = s.length() - 1;
-//	bool haveUniqueChar = false;
-//	for (auto &ch : s)
-//		mp[ch]++;
-//
-//	for (auto &x : mp)
-//	{
-//		if (x.second == 1)
-//		{
-//			haveUniqueChar = true;
-//			if (pos > s.find(x.first))
-//				pos = s.find(x.first);
-//		}
-//	}
-//
-//	if (haveUniqueChar)
-//		return pos;
-//
-//	return -1;
-//}
-
-//int firstUniqChar(std::string s) {
-//	std::unordered_map<char, int> mp;
-//	for (auto& ch : s)
-//		mp[ch]++;
-//
-//	for (int i = 0; i < s.length(); i++)
-//		if (mp.find(s[i])->second == 1)
-//			return i;
-//
-//	return -1;
-//}
-
-//https://leetcode.com/problems/first-unique-character-in-a-string/solutions/2976235/easy-c-solution-basic-approach-array-loop/
-// O(n) O(n)
-int firstUniqChar(std::string s) {
-	/*std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);*/
-
-	int arr[26] = { 0 };
-
-	// arr = {'a' -> 'z'};
-	for (int i = 0; i < s.length(); i++)
-		arr[s[i] - 'a']++;
+char repeatedCharacter(std::string s) {
+	std::unordered_set<char> seen;
 
 	for (int i = 0; i < s.length(); i++)
-		if (arr[s[i] - 'a'] == 1)
-			return i;
+	{
+		if (seen.count(s[i]))
+			return s[i];
+		else
+			seen.insert(s[i]);
+	}
 
-	return -1;
+	return {};
 }
 
 
 int main()
 {
-	std::string s = "leetcode";
-	std::cout << firstUniqChar(s) << '\n';
+	std::string s = "abccbaacz";
+	std::cout << repeatedCharacter(s) << '\n';
 
-	s = "loveleetcode";
-	std::cout << firstUniqChar(s) << '\n';
-
-	s = "aabb";
-	std::cout << firstUniqChar(s) << '\n';
-
+	s = "abcdd";
+	std::cout << repeatedCharacter(s) << '\n';
 
 	return 0;
 }
 
+/*std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);*/

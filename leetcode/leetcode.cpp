29c0be37246ed4f80e4a8100f8a87c8fbb4d3538
vinhbhn <bvinhhn@gmail.com>
2023-09-28 16:08:54 +0700

    easy: 2413. smallest even multiple

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -10,46 +10,24 @@
 #include <unordered_set>
 #include <set>
 
-//int findGCD(std::vector<int>& nums) {
-//	auto [min, max] = std::minmax_element(nums.begin(), nums.end());
-//	int greatest = 1;
-//
-//	for (int i = 1; i <= *min; i++)
-//		if ((*min % i == 0) && (*max % i == 0))
-//			greatest = i;
-//
-//	return greatest;
-//}
-
-// https://leetcode.com/problems/find-greatest-common-divisor-of-array/solutions/1419750/java-c-solution-using-euclidean-algorithm/
-// Euclid's Algorithm
-int gcd(int a, int b)
-{
-	if (b == 0) return a;
-	return gcd(b, a % b);
-}
-int findGCD(std::vector<int>& nums) {
-	int min = 1001, max = 0;
-	// find the min and max from array
-	for (int num : nums) {
-		if (num < min) min = num;
-		if (num > max) max = num;
-	}
-	return gcd(min, max);
+int smallestEvenMultiple(int n) {
+	if (n % 2 == 0)
+		return n;
+	else
+		return n * 2;
 }
 
 
 int main()
 {
-	std::vector nums = { 2,5,6,9,10 };
-	std::cout << findGCD(nums) << '\n';
+	std::string str1 = "ABCABC", str2 = "ABC";
+	std::cout << gcdOfStrings(str1, str2) << '\n';
 
-	nums = { 7,5,6,8,3 };
-	std::cout << findGCD(nums) << '\n';
+	str1 = "ABABAB", str2 = "ABAB";
+	std::cout << gcdOfStrings(str1, str2) << '\n';
 
-	nums = { 3,3 };
-	std::cout << findGCD(nums) << '\n';
-
+	str1 = "LEET", str2 = "CODE";
+	std::cout << gcdOfStrings(str1, str2) << '\n';
 
 	return 0;
 }

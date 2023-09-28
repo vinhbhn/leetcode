580230e4727044f4c336e9341ac3cb4e60df3134
vinhbhn <bvinhhn@gmail.com>
2023-09-28 21:42:17 +0700

    easy: 922. sort array by parity II

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -10,72 +10,57 @@
 #include <unordered_set>
 #include <set>
 
-// even and odd Indices
-// O(N logN) O(N)
-//std::vector<int> sortEvenOdd(std::vector<int>& nums) {
-//	std::vector<int> even, odd, result;
-//	for (int i = 0; i < nums.size(); i++)
-//	{
-//		if (i % 2 == 0)
-//			even.push_back(nums[i]);
-//		else
-//			odd.push_back(nums[i]);
-//	}
-//
-//	std::sort(even.begin(), even.end());
-//	std::sort(odd.begin(), odd.end(), std::greater());
-//
-//	int i = 0, j = 0;
-//	while (i < even.size() || j < odd.size())
-//	{
-//		if (i < even.size())
-//			result.push_back(even[i++]);
-//		if (j < odd.size())
-//			result.push_back(odd[j++]);
-//	}
-//
-//	return result;
-//}
-
-//https://leetcode.com/problems/sort-even-and-odd-indices-independently/solutions/1751131/multiple-approaches-c-fast-solution/
-// O(N^2) O(1)
-std::vector<int> sortEvenOdd(std::vector<int>& nums) {
-	int minIndex = 0; 
-	// Here in this nested loop, we are sorting the elements at even indices in non-decreasing order.
-	for (int i = 0; i < nums.size(); i += 2)
+// O(N) O(N)
+std::vector<int> sortArrayByParityII(std::vector<int>& nums) {
+	std::vector<int> even, odd, result;
+	for (int i = 0; i < nums.size(); i++)
 	{
-		minIndex = i;
-		for (int j = i + 2; j < nums.size(); j += 2) {
-			if (nums[j] < nums[minIndex])
-				minIndex = j;
-		}
-		std::swap(nums[i], nums[minIndex]);
-	}
-	// Here, we are trying to sort the elements at odd indices in non-increasing order.
-	for (int i = 1; i < nums.size(); i += 2)
-	{
-		minIndex = i;
-		for (int j = i + 2; j < nums.size(); j += 2)
-		{
-			if (nums[j] > nums[minIndex])
-				minIndex = j;
-		}
-		std::swap(nums[i], nums[minIndex]);
+		if (nums[i] % 2 == 0)
+			even.push_back(nums[i]);
+		else
+			odd.push_back(nums[i]);
 	}
 
-	return nums;
+	int i = 0, j = 0;
+	while (i < even.size() || j < odd.size())
+	{
+		if (i < even.size())
+			result.push_back(even[i++]);
+		if (j < odd.size())
+			result.push_back(odd[j++]);
+	}
+
+	return result;
 }
+
+// https://leetcode.com/problems/sort-array-by-parity-ii/solutions/1490847/c-two-pointers-solution/
+// using two pointers O(N) O(1)
+//std::vector<int> sortArrayByParityII(std::vector<int>& nums) {
+//	int n = nums.size();
+//	int i = 0, j = 1;
+//	while (i < n && j < n)
+//	{
+//		if (nums[i] % 2 == 0)
+//			i += 2;
+//		else if (nums[j] % 2 == 1)
+//			j += 2;
+//		else
+//			std::swap(nums[i], nums[j]);
+//	}
+//
+//	return nums;
+//}
 
 int main()
 {
-	std::vector<int>  nums = { 4, 1, 2, 3 };
-	std::vector<int> ans = sortEvenOdd(nums);
+	std::vector<int>  nums = { 4, 2, 5, 7 };
+	std::vector<int> ans = sortArrayByParityII(nums);
 	for (auto num : ans)
 		std::cout << num << ' ';
 	std::cout << '\n';
 
-	nums = { 2,1 };
-	ans = sortEvenOdd(nums);
+	nums = { 2,3 };
+	ans = sortArrayByParityII(nums);
 	for (auto num : ans)
 		std::cout << num << ' ';
 	std::cout << '\n';
@@ -83,5 +68,5 @@ int main()
 	return 0;
 }
 
-/*std::ios_base::sync_with_stdio(false);
-	std::cin.tie(NULL);*/
+/*ios_base::sync_with_stdio(false);
+	cin.tie(NULL);*/

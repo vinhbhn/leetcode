5adaa29d289cbf79209a9e4e944728b893008372
vinhbhn <bvinhhn@gmail.com>
2023-09-28 14:56:32 +0700

    easy: 1979. find greatest common divisor of array

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -10,26 +10,46 @@
 #include <unordered_set>
 #include <set>
 
-bool isThree(int n) {
-	// 0 1 2 3 false
-	if (n < 4)
-		return false;
+//int findGCD(std::vector<int>& nums) {
+//	auto [min, max] = std::minmax_element(nums.begin(), nums.end());
+//	int greatest = 1;
+//
+//	for (int i = 1; i <= *min; i++)
+//		if ((*min % i == 0) && (*max % i == 0))
+//			greatest = i;
+//
+//	return greatest;
+//}
 
-	int count = 1; // always n % n == 0
-
-	for (int i = 1; i <= n/2; i++)
-		if (n % i == 0)
-			++count;
-
-	return (count == 3);
+// https://leetcode.com/problems/find-greatest-common-divisor-of-array/solutions/1419750/java-c-solution-using-euclidean-algorithm/
+// Euclid's Algorithm
+int gcd(int a, int b)
+{
+	if (b == 0) return a;
+	return gcd(b, a % b);
+}
+int findGCD(std::vector<int>& nums) {
+	int min = 1001, max = 0;
+	// find the min and max from array
+	for (int num : nums) {
+		if (num < min) min = num;
+		if (num > max) max = num;
+	}
+	return gcd(min, max);
 }
 
 
 int main()
 {
-	std::cout << std::boolalpha;
-	std::cout << isThree(2) << '\n';
-	std::cout << isThree(4) << '\n';
+	std::vector nums = { 2,5,6,9,10 };
+	std::cout << findGCD(nums) << '\n';
+
+	nums = { 7,5,6,8,3 };
+	std::cout << findGCD(nums) << '\n';
+
+	nums = { 3,3 };
+	std::cout << findGCD(nums) << '\n';
+
 
 	return 0;
 }

3408977628a505c8254d8299a7fda079a93a6e48
vinhbhn <bvinhhn@gmail.com>
2023-09-29 15:29:38 +0700

    easy: 896. monotonic array

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -10,60 +10,35 @@
 #include <unordered_set>
 #include <set>
 
-// O(N) O(N)
-std::vector<int> sortArrayByParityII(std::vector<int>& nums) {
-	std::vector<int> even, odd, result;
-	for (int i = 0; i < nums.size(); i++)
-	{
-		if (nums[i] % 2 == 0)
-			even.push_back(nums[i]);
-		else
-			odd.push_back(nums[i]);
-	}
+bool isMonotonic(std::vector<int>& nums) {
+	std::vector<int> incre = nums, decre = nums;
 
-	int i = 0, j = 0;
-	while (i < even.size() || j < odd.size())
-	{
-		if (i < even.size())
-			result.push_back(even[i++]);
-		if (j < odd.size())
-			result.push_back(odd[j++]);
-	}
+	std::sort(incre.begin(), incre.end());
+	if (nums == incre)
+		return true;
 
-	return result;
+	std::sort(decre.begin(), decre.end(), std::greater());
+	if (nums == decre)
+		return true;
+
+	return false;
 }
 
-// https://leetcode.com/problems/sort-array-by-parity-ii/solutions/1490847/c-two-pointers-solution/
-// using two pointers O(N) O(1)
-//std::vector<int> sortArrayByParityII(std::vector<int>& nums) {
-//	int n = nums.size();
-//	int i = 0, j = 1;
-//	while (i < n && j < n)
-//	{
-//		if (nums[i] % 2 == 0)
-//			i += 2;
-//		else if (nums[j] % 2 == 1)
-//			j += 2;
-//		else
-//			std::swap(nums[i], nums[j]);
-//	}
-//
-//	return nums;
-//}
+bool isMonotonic(std::vector<int>& nums) {
+	return std::is_sorted(nums.begin(), nums.end()) || std::is_sorted(nums.begin(), nums.end(), std::greater<int>());
+}
 
 int main()
 {
-	std::vector<int>  nums = { 4, 2, 5, 7 };
-	std::vector<int> ans = sortArrayByParityII(nums);
-	for (auto num : ans)
-		std::cout << num << ' ';
-	std::cout << '\n';
+	std::vector<int>  nums = { 1,2,2,3 };
+	std::cout << std::boolalpha;
+	std::cout << isMonotonic(nums) << '\n';
 
-	nums = { 2,3 };
-	ans = sortArrayByParityII(nums);
-	for (auto num : ans)
-		std::cout << num << ' ';
-	std::cout << '\n';
+	nums = { 6,5,4,4 };
+	std::cout << isMonotonic(nums) << '\n';
+
+	nums = { 1,3,2 };
+	std::cout << isMonotonic(nums) << '\n';
 
 	return 0;
 }

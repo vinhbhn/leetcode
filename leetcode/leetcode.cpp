f0e621a03585b83110238ff912e3d17dc13484b6
vinhbhn <bvinhhn@gmail.com>
2023-09-30 21:38:51 +0700

    easy: 914. X of kind in a deck of cards

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -9,30 +9,37 @@
 #include <cstdint>
 #include <unordered_set>
 #include <set>
+#include <numeric>
 
-bool checkIfExist(std::vector<int>& arr) {
-	for (int i = 0; i < arr.size() - 1; i++)
-	{
-		for (int j = i + 1; j < arr.size(); j++)
-			if (arr[i] == (arr[j] * 2) || (arr[i] * 2) == arr[j])
-				return true;
-	}
+// use gcd O(N) O(N)
+bool hasGroupsSizeX(std::vector<int>& deck) {
+	std::unordered_map<int, int> mp;
+	for (auto card : deck)
+		mp[card]++;
 
-	return false;
+	int gcd_val = mp.begin()->second;
+	for (auto it = mp.begin(); it != mp.end(); it++)
+		// std::gcd in numeric header
+		gcd_val = std::gcd(gcd_val, it->second); // doing gcd with the frequency of all the cards
+	return gcd_val != 1; // if gcd is not equal to 1 it means we can create groups of size >= 2.
 }
 
 int main()
 {
 	std::cout << std::boolalpha;
 
-	std::vector nums = { 10,2,5,3 };
-	std::cout << checkIfExist(nums) << '\n';
+	std::vector nums = { 1,2,3,4,4,3,2,1 };
+	std::cout << hasGroupsSizeX(nums) << '\n';
 
-	nums = { 3,1,7,11 };
-	std::cout << checkIfExist(nums) << '\n';
+	nums = { 1,1,1,2,2,2,3,3 };
+	std::cout << hasGroupsSizeX(nums) << '\n';
 
-	nums = { 7, 1, 14, 11 };
-	std::cout << checkIfExist(nums) << '\n';
+	nums = { 0,0,0,1,1,1,2,2,2 };
+	std::cout << hasGroupsSizeX(nums) << '\n';
+
+	nums = { 1,1,2,2,2,2 };
+	std::cout << hasGroupsSizeX(nums) << '\n';
+
 
 	return 0;
 }

fde699bc521b55a5c5d36d249cc89b95963a5105
vinhbhn <bvinhhn@gmail.com>
2023-10-02 12:03:24 +0700

    easy: 2810. faulty keyboard

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -12,39 +12,26 @@
 #include <numeric>
 #include <sstream>
 
-// https://leetcode.com/problems/reverse-string-ii/solutions/2627754/c-world-s-fastest-solution-with-approach/
-// using two pointers O(n) O(n)
-//std::string reverseStr(std::string s, int k) {
-//	std::size_t l = 0, r = std::min((std::size_t)k, s.length());
-//	while (l < s.length())
-//	{
-//		std::reverse(s.begin() + l, s.begin() + r);
-//		l += (std::size_t)2 * k;
-//		r = std::min((std::size_t)k + l, s.length());
-//	}
-//
-//	return s;
-//}
-
-// https://leetcode.com/problems/reverse-string-ii/solutions/3750157/easy-0ms-solution-c/
-std::string reverseStr(std::string s, int k) {
-	for (int i = 0; i < s.length(); i += 2 * k)
+std::string finalString(std::string s) {
+	for (auto it = s.begin(); it != s.end();)
 	{
-		if (i + k <= s.length())
-			std::reverse(s.begin() + i, s.begin() + i + k);
+		if (*it == 'i')
+		{
+			std::reverse(s.begin(), it);
+			it = s.erase(it); //  remove 'i'
+		}
 		else
-			std::reverse(s.begin() + i, s.end());
+			++it;
 	}
 	return s;
 }
 
 
-
 int main()
 {
-	std::cout << reverseStr("abcdefg", 2) << '\n';
-	std::cout << reverseStr("abcd", 2) << '\n';
-	std::cout << reverseStr("abcdefg", 8) << '\n';
+	std::cout << finalString("string") << '\n';
+	std::cout << finalString("poiinter") << '\n';
+
 	return 0;
 }
 

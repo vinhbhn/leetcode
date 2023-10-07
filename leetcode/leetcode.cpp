498b728563224c73603d7c30b6d89d9b30f4fa93
vinhbhn <bvinhhn@gmail.com>
2023-10-07 19:03:39 +0700

    easy: 1071. greatest common divisor of strings

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -12,15 +12,50 @@
 #include <numeric>
 #include <sstream>
 
-bool divisorGame(int n) {
-	return (n % 2 == 0);
+//std::string gcdOfStrings(std::string str1, std::string str2) {
+//	std::string gcd = "", m_str2 = "";
+//
+//	// minimize string 2
+//	std::unordered_set<char> st;
+//	for (auto ch : str2)
+//		st.insert(ch);
+//	for (auto x : st)
+//		m_str2 += x;
+//
+//	// because str1 = str2 + str2 + ... 
+//	// so after minimize str1 = m_str2 + m_str2 + ...
+//	// so str1 % m_str2 == 0
+//	int k = 0;
+//	if (str1.length() % m_str2.length() == 0)
+//		k = str1.length() / m_str2.length();
+//	else
+//		return "";
+//
+//	for (int i = 1; i <= k; i++)
+//	{
+//		gcd += m_str2;
+//	}
+//
+//	if (gcd == str1)
+//		return m_str2;
+//	else
+//		return "";
+//
+//	return gcd;
+//}
+
+// https://leetcode.com/problems/greatest-common-divisor-of-strings/solutions/3124940/c-one-line-beats-100-runtime-explanation/
+std::string gcdOfStrings(std::string str1, std::string str2) {
+	return (str1 + str2 == str2 + str1) ? str1.substr(0, std::gcd(size(str1), size(str2))) : "";
 }
+
 
 int main()
 {
-	std::cout << std::boolalpha;
-	std::cout << divisorGame(2) << '\n';
-	std::cout << divisorGame(3) << '\n';
+	std::cout << gcdOfStrings("ABCABC", "ABC") << '\n';
+	std::cout << gcdOfStrings("ABABAB", "ABAB") << '\n';
+	std::cout << gcdOfStrings("LEET", "CODE") << '\n';
+
 
 	return 0;
 }

0ee86fc11a5a8c341d8ad71a48e4d2aad2b1a8f3
vinhbhn <bvinhhn@gmail.com>
2023-10-08 19:48:47 +0700

    easy: 1051. height checker

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -12,50 +12,31 @@
 #include <numeric>
 #include <sstream>
 
-//std::string gcdOfStrings(std::string str1, std::string str2) {
-//	std::string gcd = "", m_str2 = "";
-//
-//	// minimize string 2
-//	std::unordered_set<char> st;
-//	for (auto ch : str2)
-//		st.insert(ch);
-//	for (auto x : st)
-//		m_str2 += x;
-//
-//	// because str1 = str2 + str2 + ... 
-//	// so after minimize str1 = m_str2 + m_str2 + ...
-//	// so str1 % m_str2 == 0
-//	int k = 0;
-//	if (str1.length() % m_str2.length() == 0)
-//		k = str1.length() / m_str2.length();
-//	else
-//		return "";
-//
-//	for (int i = 1; i <= k; i++)
-//	{
-//		gcd += m_str2;
-//	}
-//
-//	if (gcd == str1)
-//		return m_str2;
-//	else
-//		return "";
-//
-//	return gcd;
-//}
+int heightChecker(std::vector<int>& heights) {
+	std::vector<int> expected = heights;
+	int count = 0;
 
-// https://leetcode.com/problems/greatest-common-divisor-of-strings/solutions/3124940/c-one-line-beats-100-runtime-explanation/
-std::string gcdOfStrings(std::string str1, std::string str2) {
-	return (str1 + str2 == str2 + str1) ? str1.substr(0, std::gcd(size(str1), size(str2))) : "";
+	std::sort(expected.begin(), expected.end());
+
+	for (int i = 0; i < heights.size(); i++)
+	{
+		if (heights[i] != expected[i])
+			++count;
+	}
+
+	return count;
 }
-
 
 int main()
 {
-	std::cout << gcdOfStrings("ABCABC", "ABC") << '\n';
-	std::cout << gcdOfStrings("ABABAB", "ABAB") << '\n';
-	std::cout << gcdOfStrings("LEET", "CODE") << '\n';
+	std::vector heights{ 1,1,4,2,1,3 };
+	std::cout << heightChecker(heights) << '\n';
 
+	heights = { 5,1,2,3,4 };
+	std::cout << heightChecker(heights) << '\n';
+
+	heights = { 1,2,3,4,5 };
+	std::cout << heightChecker(heights) << '\n';
 
 	return 0;
 }

3c7448174c148635902edce3b419485548634d23
vinhbhn <bvinhhn@gmail.com>
2023-10-09 20:14:29 +0700

    easy: 1748. sum of unique elements

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -12,48 +12,17 @@
 #include <numeric>
 #include <sstream>
 
-// slow because use vector to save sum
-//double findMaxAverage(std::vector<int>& nums, int k) {
-//	std::vector<int> result;
-//	int sum = 0, count = 0, j = 0;
-//	for (int i = 0; i <= nums.size() - k; i++)
-//	{
-//		j = i;
-//		while (count < k)
-//		{
-//			sum += nums[j++];
-//			++count;
-//		}
-//
-//		// reset
-//		result.push_back(sum);
-//		sum = 0;
-//		count = 0;
-//	}
-//
-//	int max = INT_MIN;
-//	for (auto x : result)
-//		if (max < x)
-//			max = x;
-//
-//	return static_cast<double>(max) / k;
-//}
+int sumOfUnique(std::vector<int>& nums) {
+	std::unordered_map<int, int> mp;
+	for (auto num : nums)
+		mp[num]++;
 
-double findMaxAverage(std::vector<int>& nums, int k) {
-	int sum = 0, i = 0;
+	int sum = 0;
+	for (auto &x : mp)
+		if (x.second == 1)
+			sum += x.first;
 
-	// init
-	while (i < k)
-		sum += nums[i++];
-
-	int max_sum = sum;
-	for(i = k; i < nums.size(); i++)
-	{
-		sum += nums[i] - nums[i-k];
-		max_sum = std::max(sum, max_sum);
-	}
-
-	return static_cast<double>(max_sum) / k;
+	return sum;
 }
 
 int main()

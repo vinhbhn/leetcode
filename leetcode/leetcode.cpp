f5c1e14d9c9a8390b75bc3e6ac300d3a6d8aa068
vinhbhn <bvinhhn@gmail.com>
2023-10-10 21:37:53 +0700

    easy: 1941. check if all characters have equal number of occurrences

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -13,62 +13,29 @@
 #include <sstream>
 #include <queue>
 
-//int countPoints(std::string rings) {
-//	int sum = 0;
-//	std::string numbers = "0123456789";
-//	bool r = false, g = false, b = false;
-//	for (auto rod : numbers)
-//	{
-//		for (std::string::size_type j = 1; j < rings.length(); j += 2)
-//		{
-//			if (rings[j] == rod)
-//			{
-//				if (rings[j - 1] == 'R')
-//					r = true;
-//				if (rings[j - 1] == 'G')
-//					g = true;
-//				if (rings[j - 1] == 'B')
-//					b = true;
-//			}
-//		}
-//
-//		// rod have 3 rings color
-//		if (r && g && b)
-//			sum++;
-//
-//		// reset
-//		r = false;
-//		g = false;
-//		b = false;
-//	}
-//
-//	return sum;
-//}
-
-// web
-int countPoints(std::string rings) {
-	int count = 0;
-	std::unordered_map<char, std::vector<char>> mp;
-	for (std::string::size_type i = 0; i < rings.size(); i += 2)
+bool areOccurrencesEqual(std::string s) {
+	std::unordered_map<char, int> mp;
+	for (auto ch : s)
+		mp[ch]++;
+	
+	auto it = mp.begin();
+	auto sum = it->second;
+	it++;
+	for (; it != mp.end(); it++)
 	{
-		mp[rings[i+1]].push_back(rings[i]);
-	}
-	for (auto &it : mp)
-	{
-		std::set<char> st(it.second.begin(), it.second.end());
-		count += (st.size() == 3);
+		if (sum != it->second)
+			return false;
 	}
 
-	return count;
+	return true;
 }
 
 
 int main()
 {
 	std::cout << std::boolalpha;
-	std::cout << countPoints("B0B6G0R6R0R6G9") << '\n';
-	std::cout << countPoints("B0R0G0R9R0B0G0") << '\n';
-	std::cout << countPoints("G4") << '\n';
+	std::cout << areOccurrencesEqual("abacbc") << '\n';
+	std::cout << areOccurrencesEqual("aaabb") << '\n';
 
 
 	return 0;

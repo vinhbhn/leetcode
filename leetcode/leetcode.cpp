69b5b594d9c16582b231c6c4de15eb4e52e28520
vinhbhn <bvinhhn@gmail.com>
2023-10-11 15:29:14 +0700

    easy: 1588. sum of all odd length subarrays

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -13,21 +13,21 @@
 #include <sstream>
 #include <queue>
 
-bool areOccurrencesEqual(std::string s) {
-	std::unordered_map<char, int> mp;
-	for (auto ch : s)
-		mp[ch]++;
-	
-	auto it = mp.begin();
-	auto sum = it->second;
-	it++;
-	for (; it != mp.end(); it++)
+// https://leetcode.com/problems/sum-of-all-odd-length-subarrays/solutions/3026209/c-o-n-readable-code-beats-100/
+// O(n) O(1)
+int sumOddLengthSubarrays(std::vector<int>& arr) {
+	int sum = 0, left = 0, right = 0, total = 0, odd = 0;
+
+	for (int i = 0; i < arr.size(); i++)
 	{
-		if (sum != it->second)
-			return false;
+		left = i + 1; // subarrays ending with arr[i]
+		right = arr.size() - i; // subarray starting with arr[i]
+		total = left * right; // total times the arr[i] appears in all subarrays
+		odd = (total + 1) / 2; // no. of times arr[i] appears in odd length subarrays
+		sum += odd * arr[i]; // then we multiply it with arr[i] to get the sum of all arr[i] appearences in odd length subarrays
 	}
 
-	return true;
+	return sum;
 }
 
 

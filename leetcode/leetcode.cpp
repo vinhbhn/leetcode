f82c7a3ac3a82011f511a16ebbbe5b5236a3633c
vinhbhn <bvinhhn@gmail.com>
2023-10-11 22:30:19 +0700

    easy: 415. add strings

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -14,42 +14,56 @@
 #include <queue>
 #include<map>
 
-// O(N logN)
-//std::vector<int> targetIndices(std::vector<int>&nums, int target) {
-//	std::sort(nums.begin(), nums.end());
-//
-//	std::vector<int> result;
-//	for (int i = 0; i < nums.size(); i++)
-//	{
-//		if (nums[i] == target)
-//			result.push_back(i);
-//	}
-//
-//	return result;
-//}
+//https://leetcode.com/problems/add-strings/solutions/2808140/c-grade-1-addition-technique/
+std::string addStrings(std::string num1, std::string num2) {
+	// accessing last digit of the num1 string and num2 string
+	int i = num1.size() - 1, j = num2.size() - 1;
 
-//https://leetcode.com/problems/find-target-indices-after-sorting-array/solutions/1609499/c-two-methods-with-explanation-o-n-and-o-n-log-n-approach-easy-to-understand/
-// O(N)
-std::vector<int> targetIndices(std::vector<int>& nums, int target) {
-	int small = 0, count = 0, k = 0;
-	for (auto n : nums)
+	// declared a integer variable carry and assigned it 0
+	int carry = 0;
+
+	// declared an empty string
+	std::string ans = "";
+
+	// traversing the num1 and num2 string till the leftmost digit
+	// and till carry > 0
+	while (i >= 0 || j >= 0 || carry)
 	{
-		if (n < target) small++; // calculates the frequency of elements lesser than the target
-		if (n == target) count++; // calculate the number of occurences of the target element
+		// declared an int var sum and assigned 0 to it
+		long sum = 0;
+
+		// convert the char to int and add it to sum variable
+		if (i >= 0)
+			sum += num1[i--] - '0';
+		if (j >= 0)
+			sum += num2[j--] - '0';
+
+		// add carry to the sum
+		sum += carry;
+		// carry is always the leftmost digit of a number
+		carry = sum / 10;
+
+		// concatenate the last digit of the sum after converting
+		// it into string using to_string function
+		ans += std::to_string(sum % 10);
 	}
-	std::vector<int> result(count);
-	for (int i = small; i < small + count; i++)
-	{
-		result[k++] = i;
-	}
-	return result;
+
+	// as the required sum is obtained in the reverse manner,
+	// reverse the sum to obtain the actual answer
+	std::reverse(ans.begin(), ans.end());
+	return ans;
 }
+
 
 int main()
 {
-	std::vector<int> arr = { 37,12,28,9,100,56,80,5,12 };
-	for (auto& x : arrayRankTransform(arr))
-		std::cout << x << ' ';
+	std::cout << addStrings("11", "123") << '\n';
+	std::cout << addStrings("456", "77") << '\n';
+	std::cout << addStrings("0", "0") << '\n';
+	std::cout << addStrings("1", "9") << '\n';
+	std::cout << addStrings("9", "99") << '\n';
+
+
 
 
 	return 0;

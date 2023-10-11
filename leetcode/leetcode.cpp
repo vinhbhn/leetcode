358d719b981f6cd99eeaa842d1cc2b975d4f531d
vinhbhn <bvinhhn@gmail.com>
2023-10-11 20:42:55 +0700

    easy: 2089. find target indices after sorting array

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -14,165 +14,36 @@
 #include <queue>
 #include<map>
 
-// terrible
-//std::vector<int> arrayRankTransform(std::vector<int>& arr) {
-//	std::vector<int> temp = arr, result;
+// O(N logN)
+//std::vector<int> targetIndices(std::vector<int>&nums, int target) {
+//	std::sort(nums.begin(), nums.end());
 //
-//	std::sort(temp.begin(), temp.end());
-//
-//	int j = 0, c = 0;
-//	for(int i = 0; i < arr.size(); i++)
+//	std::vector<int> result;
+//	for (int i = 0; i < nums.size(); i++)
 //	{
-//		while (j < temp.size())
-//		{
-//			
-//			if (arr[i] > temp[j])
-//			{
-//				c++;
-//				// avoid two elements equal, not count
-//				if (temp[j] == temp[j + 1])
-//				{
-//					j++;
-//				}
-//				j++;
-//				
-//			}
-//			if (arr[i] == temp[j])
-//			{
-//				break;
-//			}
-//		}
-//		result.push_back(c+1);
-//		// reset
-//		j = 0;
-//		c = 0;
+//		if (nums[i] == target)
+//			result.push_back(i);
 //	}
+//
 //	return result;
 //}
 
-// slow
-//std::vector<int> arrayRankTransform(std::vector<int>& arr) {
-//	std::vector<int> temp = arr, result;
-//
-//	std::sort(temp.begin(), temp.end());
-//	// remove the elements equal
-//	auto adjacent = std::unique(temp.begin(), temp.end());
-//	temp.erase(adjacent, temp.end());
-//
-//	int j = 0;
-//	for (int i = 0; i < arr.size(); i++)
-//	{
-//		while (true)
-//		{
-//			if (arr[i] == temp[j])
-//			{
-//				result.push_back(j + 1);
-//				break;
-//			}
-//			j++;
-//		}
-//
-//		// reset
-//		j = 0;
-//
-//	}
-//	return result;
-//}
-
-// mid
-//std::vector<int> arrayRankTransform(std::vector<int>& arr) {
-//	std::vector<int> temp = arr, result;
-//
-//	std::sort(temp.begin(), temp.end());
-//	// remove the elements equal
-//	auto adjacent = std::unique(temp.begin(), temp.end());
-//	temp.erase(adjacent, temp.end());
-//
-//	for (int i = 0; i < arr.size(); i++)
-//	{
-//		auto pos = std::find(temp.begin(), temp.end(), arr[i]);
-//		result.push_back(std::distance(temp.begin(), pos) +1);
-//	}
-//	return result;
-//}
-
-// mid
-//std::vector<int> arrayRankTransform(std::vector<int>& arr) {
-//	std::vector<int> temp = arr, result;
-//
-//	std::sort(temp.begin(), temp.end());
-//	// remove the elements equal using set
-//	std::set<int> s;
-//	for (auto i : arr)
-//		s.insert(i);
-//
-//	for (int i = 0; i < arr.size(); i++)
-//	{
-//		auto pos = std::find(temp.begin(), temp.end(), arr[i]);
-//		result.push_back(std::distance(temp.begin(), pos) +1);
-//	}
-//	return result;
-//}
-
-//https://leetcode.com/problems/rank-transform-of-an-array/solutions/1677596/3-simple-c-solution/
-// using set and map
-//std::vector<int> arrayRankTransform(std::vector<int>& arr) {
-//	std::map<int, int> mp;
-//	std::set<int> s;
-//	for (int i = 0; i < arr.size(); i++)
-//		s.insert(arr[i]); // making set that contains unique element in sorted order
-//	int rank = 1;
-//	for (auto& i : s)
-//	{
-//		mp[i] = rank;
-//		rank++; // assigning rank to unique elements
-//	}
-//	for (int i = 0; i < arr.size(); i++)
-//		arr[i] = mp[arr[i]];
-//
-//	return arr;
-//}
-// using map and sorting
-//std::vector<int> arrayRankTransform(std::vector<int>& arr) {
-//	std::map<int, int> mp;
-//	std::vector<int> temp = arr;
-//	int rank = 1;
-//	// sorting the array
-//	std::sort(temp.begin(), temp.end());
-//	// deleting repeated element
-//	auto it = std::unique(temp.begin(), temp.end());
-//	temp.resize(std::distance(temp.begin(), it));
-//	// assigning rank
-//	for (int i = 0; i < temp.size(); i++)
-//	{
-//		mp[temp[i]] = rank;
-//		rank++;
-//	}
-//	for (int i = 0; i < arr.size(); i++)
-//	{
-//		arr[i] = mp[arr[i]];
-//	}
-//	
-//
-//	return arr;
-//}
-// using map
-std::vector<int> arrayRankTransform(std::vector<int>& arr) {
-	std::map<int, int> mp;
-	for (auto& i : arr)
+//https://leetcode.com/problems/find-target-indices-after-sorting-array/solutions/1609499/c-two-methods-with-explanation-o-n-and-o-n-log-n-approach-easy-to-understand/
+// O(N)
+std::vector<int> targetIndices(std::vector<int>& nums, int target) {
+	int small = 0, count = 0, k = 0;
+	for (auto n : nums)
 	{
-		mp[i] = 0;
+		if (n < target) small++; // calculates the frequency of elements lesser than the target
+		if (n == target) count++; // calculate the number of occurences of the target element
 	}
-	int rank = 0;
-	for (auto& it : mp)
+	std::vector<int> result(count);
+	for (int i = small; i < small + count; i++)
 	{
-		mp[it.first] = ++rank;
+		result[k++] = i;
 	}
-	for (int i = 0; i < arr.size(); i++)
-		arr[i] = mp[arr[i]];
-	return arr;
+	return result;
 }
-
 
 int main()
 {

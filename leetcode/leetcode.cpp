a1510513245911bd145319d60c6a34340976013c
vinhbhn <bvinhhn@gmail.com>
2023-10-15 15:22:41 +0700

    easy: 509. fibonacci number

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -15,59 +15,39 @@
 #include<map>
 #include <charconv>
 
-// 1 -> 2 -> 3 -> 5 -> 8 -> 13
-//	s[i] = s[i-1] + s[i-2]		
-// or total = prev + curr	
-// use vector to use more space
-//int climbStairs(int n) {
-//	if (n == 1) return 1;
-//	if (n == 2) return 2;
-//
-//	std::vector<int> stairs = { 0,1,2 };
-//	int i = 3;
-//	while (i <= n)
-//	{
-//		stairs.push_back(stairs.at(i - 1) + stairs.at(i - 2));
-//		i++;
-//	}
-//
-//	return stairs[stairs.size() - 1];
-//}
-//int climbStairs(int n) {
+//int fib(int n) {
+//	if (n == 0) return 0;
 //	if (n == 1) return 1;
 //
-//	int i = 3;
-//	int prev = 1, curr = 2, temp = 0;
-//	while (i <= n)
-//	{
-//		temp = prev + curr;
-//		prev = curr;
-//		curr = temp;
-//		i++;
-//	}
-//
-//	return curr;
+//	return fib(n - 1) + fib(n - 2);
 //}
+int fib(int n) {
+	if (n == 0) return 0;
+	if (n == 1) return 1;
 
-// recursion but TC: O(2^n)
-int climbStairs(int n) {
-	if (n == 0 || n == 1)
-		return 1;
+	int i = 2, prev = 0, curr = 1, temp = 0;
+	while (i <= n)
+	{
+		temp = prev + curr;
+		prev = curr;
+		curr = temp;
 
-	return climbStairs(n - 1) + climbStairs(n - 2);
+		i++;
+	}
+
+	return curr;
 }
+
 
 int main()
 {
-	std::cout << climbStairs(2) << '\n';
-	std::cout << climbStairs(3) << '\n';
-	std::cout << climbStairs(4) << '\n';
-	std::cout << climbStairs(5) << '\n';
-	std::cout << climbStairs(6) << '\n';
-	std::cout << climbStairs(7) << '\n';
-	std::cout << climbStairs(45) << '\n';
-
-
+	std::cout << fib(2) << '\n';
+	std::cout << fib(3) << '\n';
+	std::cout << fib(4) << '\n';
+	std::cout << fib(5) << '\n';
+	std::cout << fib(6) << '\n';
+	std::cout << fib(7) << '\n';
+	std::cout << fib(30) << '\n';
 
 	return 0;
 }

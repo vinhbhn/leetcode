3c08c387705be24ee57f004940e75e9b270168c6
vinhbhn <bvinhhn@gmail.com>
2023-10-15 15:52:06 +0700

    easy: 2806. account balanec after rounded purchase

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -15,43 +15,17 @@
 #include<map>
 #include <charconv>
 
-//int tribonacci(int n) {
-//	if (n == 0) return 0;
-//	if (n == 1 || n == 2) return 1;
-//
-//	int i = 3;
-//	//	n0			n1			n2
-//	int first = 0, second = 1, third = 1, temp = 0;
-//	while (i <= n)
-//	{
-//		temp = first + second + third;
-//		first = second;
-//		second = third;
-//		third = temp;
-//
-//		i++;
-//	}
-//
-//	return third;
-//}
-int tribonacci(int n) {
-	std::vector<int> tri(38, 0);
-	tri[0] = 0;
-	tri[1] = tri[2] = 1;
+int accountBalanceAfterPurchase(int purchaseAmount) {
+	int roundedAmount = std::floor((purchaseAmount + 5) / 10) * 10;
 
-	for (int i = 3; i <= n; i++)
-	{
-		tri[i] = tri[i - 1] + tri[i - 2] + tri[i - 3];
-	}
-
-	return tri[n];
+	return 100 - roundedAmount;
 }
 
 
 int main()
 {
-	std::cout << tribonacci(4) << '\n';
-	std::cout << tribonacci(25) << '\n';
+	std::cout << accountBalanceAfterPurchase(9) << '\n';
+	std::cout << accountBalanceAfterPurchase(15) << '\n';
 
 	return 0;
 }

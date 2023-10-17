768a46ac846547a2a318ea466680e386800ff204
vinhbhn <bvinhhn@gmail.com>
2023-10-17 21:34:12 +0700

    easy: 2215. find the difference of two arrays

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -15,75 +15,108 @@
 #include<map>
 #include <charconv>
 
-// using two pointers O(mlogm + nlogn) O(min(m,n))
-//std::vector<int> intersect(std::vector<int>& nums1, std::vector<int>& nums2) {
-//	std::sort(nums1.begin(), nums1.end());
-//	std::sort(nums2.begin(), nums2.end());
-//
-//	std::vector<int> result;
-//	int i = 0, j = 0;
-//	while (i < nums1.size() && j < nums2.size())
-//	{
-//		if (nums1[i] == nums2[j])
-//		{
-//			result.push_back(nums1[i]);
-//			i++;
-//			j++;
-//		}
-//		else if (nums1[i] < nums2[j])
-//			i++;
-//		else
-//			j++;
-//	}
-//
-//	return result;
-//}
-
-// https://leetcode.com/problems/intersection-of-two-arrays-ii/solutions/3397590/simple-c-hash-map-easy-to-understand/
-//std::vector<int> intersect(std::vector<int>& nums1, std::vector<int>& nums2) {
-//	std::vector<int> ans;
-//	if (nums1.size() > nums2.size())
-//		return intersect(nums2, nums1);
-//	
-//	std::unordered_map<int, int> mp;
-//	for (auto num : nums1)
-//		mp[num]++; // here we count the frequency for every element
-//
-//	for (auto number : nums2)
-//	{
-//		// if the element is found in the unordered_map we decrement the count by 1 
-//		// and push that element in output vector along with checking the condition that count > 0
-//		if (mp.find(number) != mp.end() && mp[number] > 0)
-//		{
-//			ans.push_back(number);
-//			mp[number]--;
-//		}
-//	}
-//
-//	return ans;
-//}
-
-// https://leetcode.com/problems/intersection-of-two-arrays-ii/solutions/82269/short-python-c/
-//std::vector<int> intersect(std::vector<int>& nums1, std::vector<int>& nums2) {
-//	std::unordered_map<int, int> mp;
-//	for (auto num : nums1)
-//		mp[num]++; 
-//
-//	std::vector<int> ans;
-//	for (auto number : nums2)
-//	{
-//		if (mp[number]-- > 0)
-//			ans.push_back(number);
-//	}
-//
-//	return ans;
-//}
 using namespace std;
-vector<int> intersect(vector<int>& a, vector<int>& b) {
+// slow
+//vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+//	vector<int> res1, res2;
+//	bool equal = false;
+//
+//	// check if element in nums1 exists in nums2
+//	for (auto num : nums1)
+//	{
+//		for (int i = 0; i < nums2.size(); i++)
+//		{
+//			if (num == nums2[i])
+//			{
+//				equal = true;
+//				break;
+//			}
+//		}
+//
+//		if (!equal && find(res1.begin(), res1.end(), num) == res1.end())
+//			res1.push_back(num);
+//
+//		// reset
+//		equal = false;
+//	}
+//
+//	// check if element in nums2 exists in nums1
+//	for (auto num : nums2)
+//	{
+//		for (int i = 0; i < nums1.size(); i++)
+//		{
+//			if (num == nums1[i])
+//			{
+//				equal = true;
+//				break;
+//			}
+//		}
+//
+//		if (!equal && find(res2.begin(), res2.end(), num) == res2.end())
+//			res2.push_back(num);
+//
+//		// reset
+//		equal = false;
+//	}
+//
+//	return { res1, res2 };
+//}
+
+// O(mlogm + nlogn)
+//vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+//	vector<int> res1, res2;
+//
+//	// check if element in nums1 exists in nums2
+//	for (auto num : nums1)
+//	{
+//		if (find(nums2.begin(), nums2.end(), num) == nums2.end())
+//			if (find(res1.begin(), res1.end(), num) == res1.end())
+//				res1.push_back(num);
+//	}
+//
+//	// check if element in nums2 exists in nums1
+//	for (auto num : nums2)
+//	{
+//		if (find(nums1.begin(), nums1.end(), num) == nums1.end())
+//			if (find(res2.begin(), res2.end(), num) == res2.end())
+//				res2.push_back(num);
+//	}
+//
+//	return { res1, res2 };
+//}
+
+// O(mn)
+vector<vector<int>> findDifference(vector<int>& a, vector<int>& b) {
+	unordered_set<int> set1(a.begin(), a.end());
+	unordered_set<int> set2(b.begin(), b.end());
+
+	vector<int> res1, res2;
+
+	for (auto num : set1)
+		if (!set2.contains(num))
+			res1.push_back(num);
+
+	for (auto num : set2)
+		if (!set1.contains(num))
+			res2.push_back(num);
+
+	return { res1, res2 };
+}
+
+// web: O(m+n)
+vector<vector<int>> findDifference(vector<int>& a, vector<int>& b) {
+	vector<int> res1, res2;
+
 	sort(a.begin(), a.end());
+	a.erase(unique(a.begin(), a.end()), a.end());
+
 	sort(b.begin(), b.end());
-	a.erase(set_intersection(a.begin(), a.end(), b.begin(), b.end(), a.begin()), a.end());
-	return a;
+	b.erase(unique(b.begin(), b.end()), b.end());
+
+	set_difference(a.begin(), a.end(), b.begin(), b.end(), back_inserter(res1));
+	set_difference(b.begin(), b.end(), a.begin(), a.end(), back_inserter(res2));
+
+	return { res1, res2 };
 }
 
 int main()
@@ -109,6 +142,6 @@ int main()
 	return 0;
 }
 
-/*ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);*/
+//ios_base::sync_with_stdio(false);
+//cin.tie(nullptr);
+//cout.tie(nullptr);

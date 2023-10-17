da7f5ff00bda99f4103eb079bf75a9eeb65d7d20
vinhbhn <bvinhhn@gmail.com>
2023-10-17 17:21:14 +0700

    easy: 350. intersection of two arrays II

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -15,18 +15,8 @@
 #include<map>
 #include <charconv>
 
-//std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2) {
-//	std::unordered_set<int> set1(nums1.begin(), nums1.end()), set2(nums2.begin(), nums2.end());
-//
-//	std::vector<int> result;
-//	for (auto val : set2)
-//		if (set1.contains(val))
-//			result.push_back(val);
-//
-//	return result;
-//}
-// using two pointers O(mlogm + nlogn + m + n) O(min(m,n))
-//std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2) {
+// using two pointers O(mlogm + nlogn) O(min(m,n))
+//std::vector<int> intersect(std::vector<int>& nums1, std::vector<int>& nums2) {
 //	std::sort(nums1.begin(), nums1.end());
 //	std::sort(nums2.begin(), nums2.end());
 //
@@ -46,42 +36,73 @@
 //			j++;
 //	}
 //
-//	result.erase(std::unique(result.begin(), result.end()), result.end());
-//
 //	return result;
 //}
 
-//https://leetcode.com/problems/intersection-of-two-arrays/solutions/3075714/simple-c-code-beats-95-98-and-81-17/
-std::vector<int> intersection(std::vector<int>& nums1, std::vector<int>& nums2) {
-	std::vector<int> result;
-	for (auto i : nums1)
-	{
-		// checks if the number present in "nums1" is also present in "nums2"
-		if (std::count(nums2.begin(), nums2.end(), i) > 0)
-		{
-			// checks if the number present in both "nums1" and "nums2" is already in "result" vector
-			if (std::count(result.begin(), result.end(), i) == 0)
-			{
-				result.push_back(i);
-			}
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/solutions/3397590/simple-c-hash-map-easy-to-understand/
+//std::vector<int> intersect(std::vector<int>& nums1, std::vector<int>& nums2) {
+//	std::vector<int> ans;
+//	if (nums1.size() > nums2.size())
+//		return intersect(nums2, nums1);
+//	
+//	std::unordered_map<int, int> mp;
+//	for (auto num : nums1)
+//		mp[num]++; // here we count the frequency for every element
+//
+//	for (auto number : nums2)
+//	{
+//		// if the element is found in the unordered_map we decrement the count by 1 
+//		// and push that element in output vector along with checking the condition that count > 0
+//		if (mp.find(number) != mp.end() && mp[number] > 0)
+//		{
+//			ans.push_back(number);
+//			mp[number]--;
+//		}
+//	}
+//
+//	return ans;
+//}
 
-		}
-	}
-
-	return result;
+// https://leetcode.com/problems/intersection-of-two-arrays-ii/solutions/82269/short-python-c/
+//std::vector<int> intersect(std::vector<int>& nums1, std::vector<int>& nums2) {
+//	std::unordered_map<int, int> mp;
+//	for (auto num : nums1)
+//		mp[num]++; 
+//
+//	std::vector<int> ans;
+//	for (auto number : nums2)
+//	{
+//		if (mp[number]-- > 0)
+//			ans.push_back(number);
+//	}
+//
+//	return ans;
+//}
+using namespace std;
+vector<int> intersect(vector<int>& a, vector<int>& b) {
+	sort(a.begin(), a.end());
+	sort(b.begin(), b.end());
+	a.erase(set_intersection(a.begin(), a.end(), b.begin(), b.end(), a.begin()), a.end());
+	return a;
 }
 
 int main()
 {
 	std::vector nums1 = { 1,2,2,1 };
 	std::vector nums2 = { 2,2 };
-	for (auto x : intersection(nums1, nums2))
+	for (auto x : intersect(nums1, nums2))
+		std::cout << x << ' ';
+	std::cout << '\n';
+
+	nums1 = { 1,2,2,1 };
+	nums2 = { 2 };
+	for (auto x : intersect(nums1, nums2))
 		std::cout << x << ' ';
 	std::cout << '\n';
 
 	nums1 = { 4,9,5 };
 	nums2 = { 9,4,9,8,4 };
-	for (auto x : intersection(nums1, nums2))
+	for (auto x : intersect(nums1, nums2))
 		std::cout << x << ' ';
 	std::cout << '\n';
 

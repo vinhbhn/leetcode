c07110993972f1bf8389676a53b6e895ccd1bd29
vinhbhn <bvinhhn@gmail.com>
2023-10-19 17:16:01 +0700

    easy: 2643. row with maximum ones

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,31 +17,63 @@
 
 using namespace std;
 
-int differenceOfSum(vector<int>& nums) {
-	int ele_sum = 0, d_sum = 0;
-	for (auto num : nums)
+//vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+//	vector<int> res;
+//	for (auto& x : mat)
+//	{
+//		int count = 0;
+//		for (auto i : x)
+//			if (i == 1)
+//				count++;
+//
+//		res.push_back(count);
+//	}
+//
+//	auto max = max_element(res.begin(), res.end());
+//
+//	for (int i = 0; i < res.size(); i++)
+//		if (res[i] == *max)
+//			return { i, *max };
+//
+//	return { 0, *max };
+//}
+
+vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+	vector<int> res;
+	int maxi = 0;
+	int index = 0; // if no row found still return 0
+	for (int i = 0; i < mat.size(); i++)
 	{
-		ele_sum += num;
-		while (num)
+		int count = 0;
+		for (int j = 0; j < mat[0].size(); j++)
 		{
-			d_sum += num % 10;
-			num /= 10;
+			if (mat[i][j] == 1)
+				count++;
+		}
+
+		if (count > maxi)
+		{
+			index = i;
+			maxi = count;
 		}
 	}
 
-	return abs(ele_sum - d_sum);
+	return { index, maxi };
 }
 
 int main()
 {
-	vector<int> nums = { 1, 15, 6, 3 };
-	cout << differenceOfSum(nums) << '\n';
-	nums = { 1,2,3,4 };
-	cout << differenceOfSum(nums) << '\n';
+	{
+		vector<int> nums = { {0,1}, {1,0} };
+		cout << differenceOfSum(nums) << '\n';
+		nums = { 1,2,3,4 };
+		cout << differenceOfSum(nums) << '\n';
 
-	return 0;
+		return 0;
+	}
 }
 
+//#define need_for_speed ios_base::sync_with_stdio(false); cin.tie(NULL);
 //ios_base::sync_with_stdio(false);
 //cin.tie(nullptr);
 //cout.tie(nullptr);

c97727b266ddd9301a9bb813aca811110a7eaff9
vinhbhn <bvinhhn@gmail.com>
2023-10-20 11:00:56 +0700

    easy: 2129. capitalize the title (add more solutions)

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,16 +17,83 @@
 
 using namespace std;
 
-bool detectCapitalUse(string word) {
-	int count = 0;
-	for (int i = 0; i < word.length(); i++)
+// worked
+//string capitalizeTitle(string title) {
+//    vector<string> v;
+//    stringstream ss(title);
+//    string word;
+//
+//    while (getline(ss, word, ' '))
+//        v.push_back(word);
+//
+//    //for (auto& i : v)
+//    //	std::cout << i << ' ';
+//    //std::cout << '\n';
+//
+//    string result = "";
+//    for (auto& letter : v)
+//    {
+//        if (letter.size() <= 2)
+//            transform(letter.begin(), letter.end(), letter.begin(), ::tolower);
+//        else
+//        {
+//            letter[0] = toupper(letter[0]);
+//            for (int i = 1; i < letter.size(); i++)
+//                letter[i] = tolower(letter[i]);
+//        }
+//
+//        result += letter + ' ';
+//    }
+//
+//    // remove ' ' in last string
+//    result.pop_back();
+//    return result;
+//}
+
+//https://leetcode.com/problems/capitalize-the-title/solutions/1675922/java-c-2-approaches-pictorial-explanation-faster-than-100/
+// basic idea
+//string capitalizeTitle(string title) {
+//	int len = title.length();
+//
+//	for (int i = 0; i < len; i++)
+//	{
+//		int firstIndex = i; // store the first index of the word
+//
+//		while (i < len && title[i] != ' ')
+//		{
+//			title[i] = tolower(title[i]); // converting the character at ith index to lower case only by one
+//			++i;
+//		}
+//
+//		// if word is of length greater than 2, then turn the first character of the word to upper case
+//		if (i - firstIndex > 2)
+//			title[firstIndex] = toupper(title[firstIndex]); // converting the first character of the word to upper case
+//	}
+//
+//	return title; // return the final result
+//}
+// O(n) O(1)
+string capitalizeTitle(string title) {
+	int len = title.length();
+
+	for (int i = 0; i < len; i++)
 	{
-		if (islower(word[i]))
-			count++;
+		int firstIndex = i; // store the first index of the word
+
+		while (i < len && title[i] != ' ')
+		{
+			title[i] |= 32; // converting the character at ith index to lower case only by one
+			++i;
+		}
+
+		// if word is of length greater than 2, then turn the first character of the word to upper case
+		if (i - firstIndex > 2)
+			title[firstIndex] &= ~32; // converting the first character of the word to upper case
 	}
-	//		all upper		upper only first letter								all not capital		
-	return (count == 0 || ((count == word.length() - 1) && isupper(word[0])) || count == word.length());
+
+	return title; // return the final result
 }
+
 
 
 int main()

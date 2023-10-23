82dd89370effab40274b7f3dacce58cf97dfe764
vinhbhn <bvinhhn@gmail.com>
2023-10-23 15:04:20 +0700

    easy: 2287. rearrange characters to make target string

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,73 +17,46 @@
 
 using namespace std;
 
-// worked but slow
-//int countCharacters(vector<string>& words, string chars) {
-//	unordered_map<char, int> mpc, mpw;
-//	int sum = 0;
-//
-//	for (auto ch : chars)
-//		mpc[ch]++;
-//
-//	for(int i = 0; i < words.size(); i++)
-//	{
-//		bool fail = false;
-//		for (auto c : words[i])
-//		{
-//			mpw[c]++;
-//		}
-//
-//		for (auto c : words[i])
-//		{
-//			if (mpc[c] < mpw[c])
-//			{
-//				fail = true;
-//				break;
-//			}
-//		}
-//
-//		if (!fail)
-//			sum += words[i].size();
-//
-//		// reset
-//		mpw.clear();
-//	}
-//
-//	return sum;
-//}
+int rearrangeCharacters(string s, string t) {
+	unordered_map<char, int> mps, mpt;
+	for (auto ch : s)
+		mps[ch]++;
+	for (auto ch : t)
+		mpt[ch]++;
 
-// web
-int countCharacters(vector<string>& words, string chars) {
-	int freqhash[26] = {}, res = 0;
-	for (auto x : chars)
-		++freqhash[x - 'a'];
-
-	for (auto& w : words)
-	{
-		int hash[26] = {}, match = true;
-
-		for (auto ch : w)
-		{
-			// compare character have from words but it's not present in chars
-			if (++hash[ch - 'a'] > freqhash[ch - 'a'])
-			{
-				match = false;
-				break;
-			}
-		}
-
-		if (match)
-			res += w.size();
-	}
+	int res = INT_MAX;
+	// x occurrences in s divide y occurrences in t
+	for (auto& x : mpt)
+		res = min(res, mps[x.first] / x.second);
 
 	return res;
 }
 
+// web, fastest
+int rearrangeCharacters(string s, string t) {
+	int hashS[27] = { 0 };
+	int hashT[27] = { 0 };
+
+	for (auto ch : s)
+		hashS[ch - 'a']++;
+	for (auto ch : t)
+		hashT[ch - 'a']++;
+
+	int res = INT_MAX;
+	for (auto ch : t)
+		res = min(res, hashS[ch - 'a'] / hashT[ch - 'a']);
+
+	return res;
+}
+
+
 int main()
 {
 	cout << boolalpha;
-	vector<string> words = { "cat","bt","hat","tree" };
-	cout << countCharacters(words, "atach") << '\n';
+	cout << rearrangeCharacters("ilovecodingonleetcode", "code") << '\n';
+	cout << rearrangeCharacters("abcba", "abc") << '\n';
+	cout << rearrangeCharacters("abbaccaddaeea", "aaaaa") << '\n';
+
 
 	return 0;
 

c502e4e18b2be9ebafb59000e6e9dc82644fdc35
vinhbhn <bvinhhn@gmail.com>
2023-10-25 15:48:58 +0700

    easy: 1534. count good triplets

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,17 +17,24 @@
 
 using namespace std;
 
-int differenceOfSums(int n, int m) {
-	int sum = 0;
-	for (int i = 1; i <= n; i++)
+int countGoodTriplets(vector<int>& arr, int a, int b, int c) {
+	int count = 0, n = arr.size();
+	for (int i = 0; i < n - 2; i++)
 	{
-		if (i % m != 0)
-			sum += i;
-		else
-			sum -= i;
+		for (int j = i + 1; j < n - 1; j++)
+		{
+			if (abs(arr[i] - arr[j]) <= a)
+			{
+				for (int k = j + 1; k < n; k++)
+				{
+					if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c)
+						count++;
+				}
+			}
+		}
 	}
 
-	return sum;
+	return count;
 }
 
 int main()

bf6b5ea21a9cbf3840b7ebcecbb2e27bb82f2818
vinhbhn <bvinhhn@gmail.com>
2023-10-25 21:27:37 +0700

    easy : 2485. find the pivot integer

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,54 +17,30 @@
 
 using namespace std;
 
-// 121ms 11MB
-//int countQuadruplets(vector<int>& nums) {
-//	int count = 0, n = nums.size();
-//	for (int i = 0; i < n - 3; i++)
-//	{
-//		for (int j = i + 1; j < n - 2; j++)
-//		{
-//			for (int k = j + 1; k < n - 1; k++)
-//			{
-//				for (int l = k + 1; l < n; l++)
-//				{
-//					if (nums[i] + nums[j] + nums[k] == nums[l])
-//						count++;
-//				}
-//			}
-//		}
-//	}
+// 0ms 8.7MB
+//int pivotInteger(int n) {
+//	vector<int> v(1001, 0);
+//	for (int i = 1; i <= n; i++)
+//		v[i] = v[i - 1] + i;
 //
-//	return count;
+//	int center = sqrt(v[n]);
+//
+//	return  (v[n] - v[center] == v[center - 1]) ? center : - 1;
 //}
 
-// web 4ms 10.9MB
-int countQuadruplets(vector<int>& nums) {
-		int res = 0, n = nums.size();
-		vector<int> sum(201, 0);
-		for (int i = 1; i < n - 2; i++)
-		{
-			for (int j = 0; j < i; j++)
-				sum[nums[i] + nums[j]]++;
-
-			for (int j = i + 2; j < n; j++)
-			{
-				int diff = nums[j] - nums[i + 1];
-				res = diff > 0 ? res + sum[diff] : res;
-			}
-		}
-
-		return res;
+// 3ms 6.5MB
+int pivotInteger(int n) {
+	double x = sqrt(n * (n + 1) / 2);
+	
+	return (int(x) == x) ? x : -1;
 }
+
 
 int main()
 {
 	cout << boolalpha;
-
-	cout << sumOfMultiples(7) << '\n';
-	cout << sumOfMultiples(10) << '\n';
-	cout << sumOfMultiples(9) << '\n';
-
+	cout << pivotInteger(8) << '\n';
+	
 	return 0;
 
 }

c2f2ef3c42882f39ff90d228a2b4d6e302e5ed5a
vinhbhn <bvinhhn@gmail.com>
2023-10-25 10:13:53 +0700

    easy: 2367. number of arithmetic triplets

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,14 +17,44 @@
 
 using namespace std;
 
-int sumOfMultiples(int n) {
-	int sum = 0;
-	for (int i = 3; i <= n; i++)
-		if (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)
-			sum += i;
+// worked, 4ms 8.8MB
+//int arithmeticTriplets(vector<int>& nums, int diff) {
+//	int count = 0;
+//	for (int i = 0; i < nums.size() - 2; i++)
+//	{
+//		for (int j = i + 1; j < nums.size() - 1; j++)
+//		{
+//			if (nums[j] - nums[i] == diff)
+//			{
+//				for (int k = j + 1; k < nums.size(); k++)
+//				{
+//					if (nums[k] - nums[j] == diff)
+//						count++;
+//				}
+//			}
+//		}
+//	}
+//
+//	return count;
+//}
 
-	return sum;
+//https://leetcode.com/problems/number-of-arithmetic-triplets/solutions/2392136/c-easy-hashmap-solution-with-explanation/
+// 3ms 9.6MB
+int arithmeticTriplets(vector<int>& nums, int diff) {
+	int count = 0;
+	unordered_map<int, bool> mp;
+	for (auto num : nums)
+		mp[num] = true;
+
+	// For every element say 'elm' check if there exist both number,
+	// (elm + diff) and (elm - diff) inside map. If yes then increment count
+	for (auto num : nums)
+		if (mp[num - diff] && mp[num + diff])
+			count++;
+
+	return count;
 }
+
 
 int main()
 {

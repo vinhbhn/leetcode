f2e07eeabbe356ae8e42105574181787643a77e0
vinhbhn <bvinhhn@gmail.com>
2023-10-26 16:45:50 +0700

    easy: count odd numbers in an interval range

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,111 +17,32 @@
 
 using namespace std;
 
-// longer than bottom
-string formatMonth(string m)
-{
-	string res = "";
-	if (m == "Jan")
-		res = "-01-";
-	else if (m == "Feb")
-		res = "-02-";
-	else if (m == "Mar")
-		res = "-03-";
-	else if (m == "Apr")
-		res = "-04-";
-	else if (m == "May")
-		res = "-05-";
-	else if (m == "Jun")
-		res = "-06-";
-	else if (m == "Jul")
-		res = "-07-";
-	else if (m == "Aug")
-		res = "-08-";
-	else if (m == "Sep")
-		res = "-09-";
-	else if (m == "Oct")
-		res = "-10-";
-	else if (m == "Nov")
-		res = "-11-";
-	else if (m == "Dec")
-		res = "-12-";
+// O(n) O(1) 1628ms 6.4MB
+int countOdds(int low, int high) {
+	int count = 0;
+	for (int i = low; i <= high; i++)
+		if (i % 2 != 0)
+			count++;
 
-	return res;
-}
-string formatDay(string d)
-{
-	// remove 2 words in the last
-	d.pop_back();
-	d.pop_back();
-	// or
-	//d.substr(0, d.length() - 2);
-	if (d.size() == 1)
-		d = '0' + d;
-
-	return d;
-}
-string reformatDate(string date) {
-	string temp = "", res = "";
-	vector<string> v;
-	for (auto ch : date)
-	{
-		if (ch == ' ')
-		{
-			v.push_back(temp);
-			temp = "";
-		}
-		else
-			temp += ch;
-	}
-	v.push_back(temp);
-
-	res += v[2]; // year
-	res += formatMonth(v[1]); // month
-	res += formatDay(v[0]); // day
-
-	return res;
+	return count;
 }
 
-
-
-string formatMonth(string m)
-{
-	if (m == "Jan")
-		return  "01";
-	else if (m == "Feb")
-		return  "02";
-	else if (m == "Mar")
-		return  "03";
-	else if (m == "Apr")
-		return  "04";
-	else if (m == "May")
-		return  "05";
-	else if (m == "Jun")
-		return  "06";
-	else if (m == "Jul")
-		return  "07";
-	else if (m == "Aug")
-		return  "08";
-	else if (m == "Sep")
-		return  "09";
-	else if (m == "Oct")
-		return  "10";
-	else if (m == "Nov")
-		return  "11";
-	else if (m == "Dec")
-		return  "12";
-
-	return "";
+// O(1) O(1) 2ms 6.5MB
+// Total odd number between 1 and low - 1 is low/2.
+//Total odd number between 1 and high is(high + 1) / 2.
+int countOdds(int low, int high) {
+	return ((high + 1) / 2 - (low / 2));
 }
-string reformatDate(string date) {
-	string res = "";
-	// date.length() == 13 <-> day >= 10th
-	if (date.length() == 13)
-		res = date.substr(9, 4) + '-' + formatMonth(date.substr(5, 3)) + '-' + date.substr(0, 2);
-	else
-		res = date.substr(8, 4) + '-' + formatMonth(date.substr(4, 3)) + '-' + '0' + date.substr(0, 1);
 
-	return res;
+//https://leetcode.com/problems/count-odd-numbers-in-an-interval-range/solutions/3178012/o-1-tc-super-easy-solution-c-python3-java-commented/
+// O(1) O(1) 2ms 6.3MB
+int countOdds(int low, int high) {
+	int odd = (high - low) / 2;
+
+	if (low % 2 || high % 2)
+		odd++;
+
+	return odd;
 }
 
 int main()

a6d84d5211f147f0d37d7ec25ebeb44c0a1d6082
vinhbhn <bvinhhn@gmail.com>
2023-10-27 17:34:58 +0700

    easy: 1929. concatenation of array

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,24 +17,10 @@
 
 using namespace std;
 
-int numberOfMatches(int n) {
-	int matches = 0;
-	// be careful with n == 1
-	while (n > 1)
-	{
-		if (n % 2 == 0)
-		{
-			matches += n / 2;
-			n /= 2;
-		}
-		else
-		{
-			matches += (n - 1) / 2;
-			n = (n - 1) / 2 + 1;
-		}
-	}
-
-	return matches;
+vector<int> getConcatenation(vector<int>& nums) {
+	vector<int> res = nums;
+	res.insert(res.begin(), nums.begin(), nums.end());
+	return res;
 }
 
 int main()

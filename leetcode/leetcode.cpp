141660142ef38cfc8fd8ffa0ff28a22350df6957
vinhbhn <bvinhhn@gmail.com>
2023-10-27 21:51:08 +0700

    easy: 2278. percentage of letter in string

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,65 +17,13 @@
 
 using namespace std;
 
-// 6ms 7.2MB  O(N*logN) O(n)
-//typedef pair<char, int> P;
-//string frequencySort(string s) {
-//	unordered_map<char, int> mp;
-//	for (auto& ch : s)
-//		mp[ch]++;
-//
-//	vector<P> v;
-//	for (auto& it : mp)
-//		v.push_back(it);
-//
-//	auto cmp = [&](P& a, P& b)
-//		{
-//			return (a.second == b.second) ? a.first < b.first : a.second > b.second;
-//		};
-//
-//	sort(v.begin(), v.end(), cmp);
-//
-//	string res = "";
-//	for (auto& x : v)
-//	{
-//		res += string(x.second, x.first);
-//	}
-//
-//	return res;
-//}
-
-// 109ms 8.2MB
-//string frequencySort(string s) {
-//	unordered_map<char, int> mp;
-//	for (auto& ch : s)
-//		mp[ch]++;
-//
-//	sort(s.begin(), s.end(), [&mp](char &a, char &b) 
-//		{
-//		return (mp[a] == mp[b]) ? a < b : mp[a] > mp[b];
-//		});
-//
-//	return s;
-//}
-
-// web, using priority_queue 13ms 9.2MB
-string frequencySort(string s) {
-	unordered_map<char, int> mp;
+int percentageLetter(string s, char letter) {
+	int count = 0;
 	for (auto& ch : s)
-		mp[ch]++;
+		if (ch == letter)
+			count++;
 
-	priority_queue<pair<int, char>> pq;
-	for (auto& it : mp)
-		pq.push({ it.second, it.first });
-
-	string res = "";
-	while(!pq.empty())
-	{
-		res += string(pq.top().first, pq.top().second);
-		pq.pop();
-	}
-
-	return res;
+	return (count * 100 / s.size());
 }
 
 int main()

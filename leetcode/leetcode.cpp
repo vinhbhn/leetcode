9b8713c34ac4d647f80b8542d93c8e766ed16824
vinhbhn <bvinhhn@gmail.com>
2023-10-30 20:11:08 +0700

    medium: 442. find all duplicates in an array

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,46 +17,50 @@
 
 using namespace std;
 
-// 12ms 13.4MB O(n) O(1)
-int mostFrequent(vector<int>& nums, int key) {
-	unordered_map<int, int> mp;
-	for (int i = 0; i <= nums.size() - 2; i++)
+// 117 ms 46.9MB O(nlogn) O(n)
+//vector<int> findDuplicates(vector<int>& nums) {
+//	map<int, int> mp;
+//	for (auto num : nums)
+//		mp[num]++;
+//
+//	vector<int> res;
+//	for (auto& x : mp)
+//		if (x.second >= 2)
+//			res.push_back(x.first);
+//
+//	return res;
+//}
+
+// 52ms 33.9MB O(n) O(1)
+//vector<int> findDuplicates(vector<int>& nums) {
+//	sort(nums.begin(), nums.end());
+//	vector<int> res;
+//	for (int i = 0; i < nums.size() -1;)
+//	{
+//		if (nums[i] == nums[i + 1])
+//		{
+//			res.push_back(nums[i]);
+//			i += 2;
+//		}
+//		else
+//			i++;
+//	}
+//
+//	return res;
+//}
+
+// web 19ms real 37ms 33.9MB O(n) O(1)
+vector<int> findDuplicates(vector<int>& nums) {
+	vector<int> res;
+	for (int i = 0; i < nums.size(); i++)
 	{
-		if (nums[i] == key)
-		{
-			mp[nums[i + 1]]++;
-		}
+		if (nums[abs(nums[i]) - 1] > 0)
+			nums[abs(nums[i]) - 1] *= -1;
+		else if (nums[abs(nums[i]) - 1] < 0)
+			res.push_back(abs(nums[i]));
 	}
 
-	priority_queue<pair<int, int>> pq;
-	for (auto& x : mp)
-		pq.push({ x.second, x.first });
-
-	return pq.top().second;
-}
-
-// 8ms 13MB
-int mostFrequent(vector<int>& nums, int key) {
-	unordered_map<int, int> mp;
-	for (int i = 0; i <= nums.size() - 2; i++)
-	{
-		if (nums[i] == key)
-		{
-			mp[nums[i + 1]]++;
-		}
-	}
-
-	int maxi = 0, maxf = 0;
-	for (auto& x : mp)
-	{
-		if (x.second > maxf)
-		{
-			maxf = x.second;
-			maxi = x.first;
-		}
-	}
-
-	return maxi;
+	return res;
 }
 
 int main()

115cc4366a1730da7c782d6cd35080ff0c966f69
vinhbhn <bvinhhn@gmail.com>
2023-10-31 19:54:37 +0700

    medium: 3Sum

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,91 +17,106 @@
 
 using namespace std;
 
-// 38ms 21.7MB O(nlogn) O(n)
-string shortestCompletingWord(string licensePlate, vector<string>& words) {
-	unordered_map<char, int> mpl;
-	for (auto ch : licensePlate)
+// O(n^3 log(res)) O(n) pass 308/312 cases brute force
+vector<vector<int>> threeSum(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
+
+	int n = nums.size();
+	vector<vector<int>> res;
+	for (int i = 0; i < n - 2; i++)
 	{
-		if (islower(ch) || isupper(ch))
-			mpl[tolower(ch)]++;
-	}
-
-	string res = "";
-	int len = 16;
-	for (auto &word : words)
-	{
-		bool containsAllLetter = true;
-		unordered_map<char, int> mpw;
-		for (auto ch : word)
+		for (int j = i + 1; j < n - 1; j++)
 		{
-			mpw[ch]++;
-		}
-
-		for (auto& x : mpl)
-		{
-			if (mpw[x.first] < x.second)
+			for (int k = j + 1; k < n; k++)
 			{
-				containsAllLetter = false;
-				break;
-			}
-		}
-
-		
-		if (containsAllLetter)
-		{
-			if (word.length() < len)
-			{
-				len = word.length();
-				res = word;
+				if (nums[i] + nums[j] + nums[k] == 0)
+					res.push_back({nums[i], nums[j], nums[k]});
 			}
 		}
 	}
+
+	sort(res.begin(), res.end());
+	res.erase(unique(res.begin(), res.end()), res.end());
 
 	return res;
 }
 
+// https://leetcode.com/problems/3sum/solutions/3109452/c-easiest-beginner-friendly-sol-set-two-pointer-approach-o-n-2-logn-time-and-o-n-space/
+// TC: O(n^2 logn + nlogn) - O(n^2 logn) SC: O(n)  1143ms 189.9MB using two pointer
+vector<vector<int>> threeSum(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
 
-// web 0ms real 13ms 11.9MB
-string shortestCompletingWord(string licensePlate, vector<string>& words) {
-	transform(licensePlate.begin(), licensePlate.end(), licensePlate.begin(), ::tolower);
-
-	int u[26] = { 0 };
-	string res;
-
-	for (auto ch : licensePlate)
+	int n = nums.size();
+	vector<vector<int>> res;
+	set<vector<int>> st;
+	for (int i = 0; i < n; i++)
 	{
-		if (!isalpha(ch))
-			continue;
+		int j = i + 1, k = n - 1;
 
-		u[ch - 'a']++;
-	}
-
-	for(auto &word : words)
-	{
-		int v[26] = { 0 };
-		for (auto ch : word)
-			v[ch - 'a']++;
-
-		bool containsAllLetter = true;
-		for (int i = 0; i < 26; i++)
+		while (j < k)
 		{
-			if (u[i] > v[i])
+			int sum = nums[i] + nums[j] + nums[k];
+			if (sum == 0)
 			{
-				containsAllLetter = false;
-				break;
+				st.insert({ nums[i], nums[j], nums[k] });
+				j++;
+				k--;
 			}
-		}
-
-		if (containsAllLetter)
-		{
-			if (res.length() == 0)
-				res = word;
-			else if (res.length() > word.length())
-				res = word;
+			else if (sum < 0)
+				j++;
+			else
+				k--;
 		}
 	}
+
+	for (auto& a : st)
+		res.push_back(a);
 
 	return res;
+}
+
+// https://leetcode.com/problems/3sum/solutions/1462423/c-both-two-pointers-and-hashmap-approach-explained/
+// 321ms 25.2MB using hashmap
+vector<vector<int>> threeSum(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
+	if (nums.size() < 3) // base case 1
+		return {};
+	if (nums[0] > 0) // base case 2
+		return {};
+
+	unordered_map<int, int> hashMap;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		// hashsing of indices
+		hashMap[nums[i]] = i;
+	}
+
+	vector<vector<int>> ans;
+	for (int i = 0; i < nums.size() - 2; ++i)
+	{
+		// Traversing the array to fix the number
+		if (nums[i] > 0)
+		{
+			// If number fixed is +ve, stop there because we can't make it zero by searching after it.
+			break;
+		}
+
+		for (int j = i + 1; j < nums.size(); j++)
+		{
+			// Fixing another number after first number
+			int required = -1 * (nums[i] + nums[j]); // to make sum 0, we would require the -ve sum of both fixed numbers.
+			if (hashMap.count(required) && hashMap.find(required)->second > j)
+			{
+				// If it exists in hashmap and its last occurrence index > 2nd fixed index, we found our triplet.
+				ans.push_back({ nums[i], nums[j], required });
+			}
+
+			j = hashMap.find(nums[j])->second; // Update j to last occurrence of 2nd fixed number to avoid duplicate triplets.
+		}
+
+		i = hashMap.find(nums[i])->second; // Update i to last occurrence of 1st fixed number to avoid duplicate triplets.
+	}
+	return ans;
 }
 
 int main()

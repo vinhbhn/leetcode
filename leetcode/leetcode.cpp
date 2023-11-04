0537b09a24a940f7b6b27953ee42864b69840f61
vinhbhn <bvinhhn@gmail.com>
2023-11-04 16:46:48 +0700

    easy: 1005. maximize sum of array after k negations

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,138 +17,30 @@
 
 using namespace std;
 
-//// 11ms 8.6MB O(nlogn + n/2)
-//string sortString(string s) {
-//	map<char, int> mp;
-//
-//	for (auto ch : s)
-//		mp[ch]++;
-//
-//	int count = 0, flip = 0;
-//	string res = "";
-//	while (count != s.length())
-//	{
-//		string temp = "";
-//		flip++;
-//		for (auto& x : mp)
-//		{
-//			if (x.second > 0)
-//			{
-//				temp += x.first;
-//				x.second--;
-//				count++;
-//			}
-//		}
-//
-//		if (flip % 2 == 0)
-//			reverse(temp.begin(), temp.end());
-//
-//		res += temp;
-//	}
-//
-//	return res;
-//}
-
-// 3ms 8.2MB 
-//string sortString(string s) {
-//	vector<int> v(26, 0);
-//
-//	for (auto ch : s)
-//		v[ch - 'a']++;
-//
-//	int count = 0, flip = 0;
-//	string res = "";
-//	while (count != s.length())
-//	{
-//		string temp = "";
-//		flip++;
-//		for (int i  = 0; i < v.size(); i++)
-//		{
-//			if (v[i] > 0)
-//			{
-//				temp += (i + 'a');
-//				v[i]--;
-//				count++;
-//			}
-//		}
-//
-//		if (flip % 2 == 0)
-//			reverse(temp.begin(), temp.end());
-//
-//		res += temp;
-//	}
-//
-//	return res;
-//}
-
-// 9ms 8MB O(n) O(1)
-//string sortString(string s) {
-//	vector<int> v(26, 0);
-//
-//	for (auto ch : s)
-//		v[ch - 'a']++;
-//
-//	int count = 0;
-//	string res = "";
-//	while (count != s.length())
-//	{
-//		for (int i = 0; i < v.size(); i++)
-//		{
-//			if (v[i] > 0)
-//			{
-//				res.push_back(i + 'a');
-//				v[i]--;
-//				count++;
-//			}
-//		}
-//
-//		for (int i = v.size() - 1; i >= 0; i--)
-//		{
-//			if (v[i] > 0)
-//			{
-//				res.push_back(i + 'a');
-//				v[i]--;
-//				count++;
-//			}
-//		}
-//	}
-//
-//	return res;
-//}
-
-// 6ms 7.9MB
-string sortString(string s) {
-	vector<int> v(26, 0);
-
-	for (auto ch : s)
-		v[ch - 'a']++;
-
-	int count = 0;
-	string res = "";
-	while (count != s.length())
+// 8ms 9.6MB O(n) O(1)
+int largestSumAfterKNegations(vector<int>& nums, int k) {
+	while (k--)
 	{
-		for (int i = 0; i < v.size(); i++)
-		{
-			if (v[i] > 0)
-			{
-				res += (i + 'a');
-				v[i]--;
-				count++;
-			}
-		}
-
-		for (int i = v.size() - 1; i >= 0; i--)
-		{
-			if (v[i] > 0)
-			{
-				res += (i + 'a');
-				v[i]--;
-				count++;
-			}
-		}
+		auto m = min_element(nums.begin(), nums.end());
+		*m = -*m;
 	}
 
-	return res;
+	int sum = 0;
+	for (auto num : nums)
+		sum += num;
+
+	return sum;
+}
+
+// 12ms 9.3MB
+int largestSumAfterKNegations(vector<int>& nums, int k) {
+	while (k--)
+	{
+		auto m = min_element(nums.begin(), nums.end());
+		*m = -*m;
+	}
+
+	return accumulate(nums.begin(), nums.end(), 0);
 }
 
 int main() {

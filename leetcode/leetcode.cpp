6b24be8a9d566ebc8179f2a71405863341bb0cec
vinhbhn <bvinhhn@gmail.com>
2023-11-09 15:55:01 +0700

    easy: 1502. can make arithmetic progression from sequence

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,18 +17,19 @@
 
 using namespace std;
 
-// 6ms 18.4MB O(nlogn + n) O(logn)
-int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-	sort(seats.begin(), seats.end());
-	sort(students.begin(), students.end());
+// 3ms 9.6MB O(nlogn + n) O(logn)
+bool canMakeArithmeticProgression(vector<int>& arr) {
+	int n = arr.size();
+	sort(arr.begin(), arr.end());
 
-	int res = 0;
-	for (int i = 0; i < seats.size(); i++)
+	int diff = arr[1] - arr[0];
+	for (int i = 2; i < n; i++)
 	{
-		res += abs(seats[i] - students[i]);
+		if (arr[i] - arr[i - 1] != diff)
+			return false;
 	}
 
-	return res;
+	return true;
 }
 
 int main() {

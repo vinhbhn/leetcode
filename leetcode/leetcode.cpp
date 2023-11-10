75992657286133adb189c1be2068b2f8501b5a7d
vinhbhn <bvinhhn@gmail.com>
2023-11-10 16:36:26 +0700

    easy: 1486. XOR Operation in an Array

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,68 +17,17 @@
 
 using namespace std;
 
-// 0ms 7.2MB
-bool buddyStrings(string s, string goal) {
-	// case s == goal
-	if (s == goal)
+// 0ms 6.5MB O(n) O(1)
+int xorOperation(int n, int start) {
+	int res = 0;
+	while(n)
 	{
-		for (int i = 0; i < s.size() - 1; i++)
-		{
-			for (int j = i + 1; j < s.size(); j++)
-			{
-				string temp = s;
-				swap(temp[i], temp[j]);
-
-				if (temp == goal)
-					return true;
-			}
-		}
-
-		return false;
+		res ^= start;
+		start += 2;
+		n--;
 	}
 
-	// case s != goal
-	int i = -1, j = -1;
-	for (int pos = 0; pos < s.size(); pos++)
-	{
-		if (s[pos] != goal[pos])
-		{
-			if (i == -1)
-				i = pos;
-			else
-				j = pos;
-		}
-
-		if (i != -1 && j != -1)
-			break;
-	}
-	if (i != j && i != -1 && j != -1)
-		swap(s[i], s[j]);
-	return (s == goal);
-}
-
-//https://leetcode.com/problems/buddy-strings/solutions/3711198/c-easy-explanation-self-explanatory-code-beats-100/
-// O(n) O(n) 6ms 7.4MB
-bool buddyStrings(string s, string goal) {
-	if (s.length() != goal.length() || s.length() <= 1)
-		return false;
-
-	if (s == goal)
-	{
-		unordered_set<char> distinctChars(s.begin(), s.end());
-		return distinctChars.size() < s.length();
-	}
-
-	vector<int> index;
-	for (int i = 0; i < s.length(); i++)
-		if (s[i] != goal[i])
-			index.push_back(i);
-
-	if (index.size() != 2)
-		return false;
-
-	swap(s[index[0]], s[index[1]]);
-	return s == goal;
+	return res;
 }
 
 int main() {

715c0989ca8b6b024e73fe84b8fb5167085624fd
vinhbhn <bvinhhn@gmail.com>
2023-11-10 15:19:50 +0700

    easy: 819. most common word

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,123 +17,67 @@
 
 using namespace std;
 
-// 64ms 38.9MB O(l*(m + nlogn)) O(l*(n+logn)) m = nums.size(), n = copyV.size() 
-vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-	vector<bool> res;
-	for (int i = 0; i < l.size(); i++)
-	{
-		bool noDiff = true;
-		vector<int> copyV;
-		copy(nums.begin() + l[i], nums.begin() + r[i] + 1, back_inserter(copyV));
+// 6ms 8.4MB O(n+b+m) O(logm) n = paragraph.size(), b = banned.size(), m = mp.size()
+string mostCommonWord(string paragraph, vector<string>& banned) {
+	unordered_map<string, int> mp;
 
-		int n = copyV.size();
-		if (n <= 2)
+	string temp = "";
+	paragraph += ' ';
+	for (auto ch : paragraph)
+	{
+		if (isalpha(ch))
+			temp += tolower(ch);
+		else
 		{
-			res.push_back(true);
-			continue;
+			mp[temp]++;
+			temp = "";
 		}
+	}
 
-		sort(copyV.begin(), copyV.end());
-		int moves = copyV[1] - copyV[0];
-		for (int j = 2; j < n; j++)
+	for (auto& str : banned)
+	{
+		if (mp.count(str))
+			mp.erase(str); // erase key, but value still in mp
+	}
+
+	priority_queue<pair<int, string>> pq;
+	for (auto& x : mp)
+	{
+		if (x.first != "")
+			pq.push({ x.second, x.first });
+	}
+
+	return pq.top().second;
+}
+
+// 3ms 8.1MB O(n+b+m) O(1) n = paragraph.size(), b = banned.size(), m = mp.size()
+string mostCommonWord(string paragraph, vector<string>& banned) {
+	unordered_map<string, int> mp;
+
+	string temp = "";
+	paragraph += ' ';
+	for (auto ch : paragraph)
+	{
+		if (isalpha(ch))
+			temp += tolower(ch);
+		else
 		{
-			if (copyV[j] - copyV[j - 1] != moves)
-			{
-				noDiff = false;
-				break;
-			}
+			mp[temp]++;
+			temp = "";
 		}
-
-		if (noDiff)
-			res.push_back(true);
-		else
-			res.push_back(false);
-
 	}
 
-	return res;
-}
+	for (auto& str : banned)
+		mp.erase(str); // erase key, but value still in mp
 
-// 84ms 39MB O(l*(m + nlogn)) O(l*(n+logn)) m = nums.size(), n = copyV.size(), l = l.size()
-bool canMakeArithmeticProgression(vector<int>& arr) {
-	sort(arr.begin(), arr.end());
-
-	int moves = arr[1] - arr[0];
-	for (int i = 2; i < arr.size(); i++)
+	string res = "";
+	int m = 0;
+	for (auto& x : mp)
 	{
-		if (arr[i] - arr[i - 1] != moves)
-			return false;
-	}
-
-	return true;
-}
-vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-	vector<bool> res;
-	for (int i = 0; i < l.size(); i++)
-	{
-		vector<int> copyV;
-		copy(nums.begin() + l[i], nums.begin() + r[i] + 1, back_inserter(copyV));
-
-		if (canMakeArithmeticProgression(copyV))
-			res.push_back(true);
-		else
-			res.push_back(false);
-
-	}
-
-	return res;
-}
-
-// 51ms 26.3MB O(l*nlogn) O(l*(n+logn)) n = copyV.size(), l = l.size()
-bool canMakeArithmeticProgression(vector<int>& arr) {
-	sort(arr.begin(), arr.end());
-
-	int moves = arr[1] - arr[0];
-	for (int i = 2; i < arr.size(); i++)
-	{
-		if (arr[i] - arr[i - 1] != moves)
-			return false;
-	}
-
-	return true;
-}
-vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-	vector<bool> res;
-	for (int i = 0; i < l.size(); i++)
-	{
-		vector<int> copyV(nums.begin() + l[i], nums.begin() + r[i] + 1);
-
-		if (canMakeArithmeticProgression(copyV))
-			res.push_back(true);
-		else
-			res.push_back(false);
-
-	}
-
-	return res;
-}
-
-// web 19ms real 22ms 21.4MB
-vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
-	vector<bool> res;
-	for (int i = 0, j = 0; i < l.size(); i++)
-	{
-		auto [p_min, p_max] = minmax_element(nums.begin() + l[i], nums.begin() + r[i] + 1);
-		int len = r[i] - l[i] + 1, d = (*p_max - *p_min) / (len - 1);
-		if (*p_max == *p_min)
-			res.push_back(true);
-		else if ((*p_max - *p_min) % (len - 1))
-			res.push_back(false);
-		else
+		if (x.second > m && x.first != "")
 		{
-			vector<bool> n(len);
-			for (j = l[i]; j <= r[i]; j++)
-			{
-				if ((nums[j] - *p_min) % d || n[(nums[j] - *p_min) / d])
-					break;
-				n[(nums[j] - *p_min) / d] = true;
-			}
-			res.push_back(j > r[i]);
+			m = x.second;
+			res = x.first;
 		}
 	}
 

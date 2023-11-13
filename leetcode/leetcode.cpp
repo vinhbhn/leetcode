a01886027ad370ebd5b649e3a49af5a05aa68902
vinhbhn <bvinhhn@gmail.com>
2023-11-13 16:51:08 +0700

    easy: 812. largest triangle area

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,32 +17,51 @@
 
 using namespace std;
 
-// 26ms 22.4MB O(nlogn) O(n)
-int largestPerimeter(vector<int>& nums) {
-	int res = 0;
-
-	sort(nums.begin(), nums.end());
-	for (int i = nums.size() - 1; i >= 2; i--)
+// 95ms 32.8MB O(n^3) O(n)
+double area(vector<int> a, vector<int> b, vector<int> c)
+{
+	int x1 = a[0];
+	int y1 = a[1];
+	int x2 = b[0];
+	int y2 = b[1];
+	int x3 = c[0];
+	int y3 = c[1];
+	return 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)); // shoelace's formula
+}
+double largestTriangleArea(vector<vector<int>>& p) {
+	double res = 0;
+	for (int i = 0; i < p.size() - 2; i++)
 	{
-		if (nums[i] < nums[i - 1] + nums[i - 2])
-			res = max(res, nums[i] + nums[i - 1] + nums[i - 2]);
+		for (int j = i + 1; j < p.size() - 1; j++)
+		{
+			for (int k = j + 1; k < p.size(); k++)
+			{
+				res = max(res, area(p[i], p[j], p[k]));
+			}
+		}
 	}
 
 	return res;
 }
 
-// 26ms 22.4MB O(nlogn) O(n)
-int largestPerimeter(vector<int>& nums) {
-	int res = 0;
-
-	sort(nums.begin(), nums.end());
-	for (int i = nums.size() - 1; i >= 2; i--)
+// 6ms 7.9MB O(n^3) O(n)
+double largestTriangleArea(vector<vector<int>>& p) {
+	double res = 0;
+	for (int i = 0; i < p.size() - 2; i++)
 	{
-		// after the array is sorted, 3 last element is largest of array
-		// so if 3 element is a triangle, it's the largest perimeter
-		if (nums[i] < nums[i - 1] + nums[i - 2])
-			return nums[i] + nums[i - 1] + nums[i - 2]; 
-
+		int x1 = p[i][0];
+		int y1 = p[i][1];
+		for (int j = i + 1; j < p.size() - 1; j++)
+		{
+			int x2 = p[j][0];
+			int y2 = p[j][1];
+			for (int k = j + 1; k < p.size(); k++)
+			{
+				int x3 = p[k][0];
+				int y3 = p[k][1];
+				res = max(res, 0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))); // shoelace's formula
+			}
+		}
 	}
 
 	return res;

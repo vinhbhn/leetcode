96615bea1d37d0f591eddfbd22a8e9fcb194a80f
vinhbhn <bvinhhn@gmail.com>
2023-11-14 17:18:14 +0700

    medium: 1930. unique length-3 palindromic subsequences

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,14 +17,72 @@
 
 using namespace std;
 
-// 3ms 8.9MB O(n)
-int numOfStrings(vector<string>& patterns, string word) {
+// 251ms 30.9MB
+//int countPalindromicSubsequence(string s) {
+//	int count = 0;
+//
+//	for (char alpha = 'a'; alpha <= 'z'; alpha++)
+//	{
+//		auto first = s.find(alpha);
+//		auto last = s.find_last_of(alpha);
+//		if (first < last)
+//		{
+//			// count character inside left and right, eg: aXa
+//			string temp = s.substr(first + 1, last - first - 1);
+//			unordered_set<char> st;
+//			for (auto& ch : temp)
+//				st.insert(ch);
+//			count += st.size();
+//		}
+//	}
+//
+//	return count;
+//}
+
+// 94ms 29.1MB O(26 * (t+a)) O(26 * t), t = temp.size(), a = alphabets.size()
+//int countPalindromicSubsequence(string s) {
+//	int count = 0;
+//
+//	for (char alpha = 'a'; alpha <= 'z'; alpha++)
+//	{
+//		auto first = s.find(alpha);
+//		auto last = s.find_last_of(alpha);
+//		if (first < last)
+//		{
+//			// count character inside left and right, eg: aXa
+//			string temp = s.substr(first + 1, last - first - 1);
+//			vector<int> alphabets(26, 0);
+//			for (auto ch : temp)
+//				alphabets[ch-'a']++;
+//
+//			for (int i = 0; i < alphabets.size(); i++)
+//				if (alphabets[i] > 0)
+//					count++;
+//		}
+//	}
+//
+//	return count;
+//}
+
+// 131ms 28.9MB
+int countPalindromicSubsequence(string s) {
 	int count = 0;
-	for (auto& p : patterns)
+
+	for (char alpha = 'a'; alpha <= 'z'; alpha++)
 	{
-		if (word.find(p) != string::npos)
+		auto first = s.find(alpha);
+		auto last = s.find_last_of(alpha);
+		if (first < last)
 		{
-			count++;
+			// count character inside left and right, eg: aXa
+			string temp = s.substr(first + 1, last - first - 1);
+			vector<bool> found(26, 0);
+			for (auto ch : temp)
+				found[ch - 'a'] = true;
+
+			for (int i = 0; i < found.size(); i++)
+				if (found[i])
+					count++;
 		}
 	}
 
@@ -33,6 +91,10 @@ int numOfStrings(vector<string>& patterns, string word) {
 
 int main() {
 	cout << boolalpha;
+	cout << countPalindromicSubsequence("aabca") << '\n';
+	cout << countPalindromicSubsequence("bbcbaba") << '\n';
+	cout << countPalindromicSubsequence("adc") << '\n';
+
 
 	return 0;
 }

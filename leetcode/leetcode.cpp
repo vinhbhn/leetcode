9f8d9b836e08eb14fe7dc4ff279c8300d569a1a8
vinhbhn <bvinhhn@gmail.com>
2023-11-15 15:56:04 +0700

    medium: 1846. maximum element after decreasing and rearranging

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,76 +17,21 @@
 
 using namespace std;
 
-// 251ms 30.9MB
-//int countPalindromicSubsequence(string s) {
-//	int count = 0;
-//
-//	for (char alpha = 'a'; alpha <= 'z'; alpha++)
-//	{
-//		auto first = s.find(alpha);
-//		auto last = s.find_last_of(alpha);
-//		if (first < last)
-//		{
-//			// count character inside left and right, eg: aXa
-//			string temp = s.substr(first + 1, last - first - 1);
-//			unordered_set<char> st;
-//			for (auto& ch : temp)
-//				st.insert(ch);
-//			count += st.size();
-//		}
-//	}
-//
-//	return count;
-//}
+// 72ms 51.6MB O(nlogn) O(n)
+int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+	// sort and check arr[0] == 1
+	sort(arr.begin(), arr.end());
+	arr[0] = 1;
 
-// 94ms 29.1MB O(26 * (t+a)) O(26 * t), t = temp.size(), a = alphabets.size()
-//int countPalindromicSubsequence(string s) {
-//	int count = 0;
-//
-//	for (char alpha = 'a'; alpha <= 'z'; alpha++)
-//	{
-//		auto first = s.find(alpha);
-//		auto last = s.find_last_of(alpha);
-//		if (first < last)
-//		{
-//			// count character inside left and right, eg: aXa
-//			string temp = s.substr(first + 1, last - first - 1);
-//			vector<int> alphabets(26, 0);
-//			for (auto ch : temp)
-//				alphabets[ch-'a']++;
-//
-//			for (int i = 0; i < alphabets.size(); i++)
-//				if (alphabets[i] > 0)
-//					count++;
-//		}
-//	}
-//
-//	return count;
-//}
-
-// 131ms 28.9MB
-int countPalindromicSubsequence(string s) {
-	int count = 0;
-
-	for (char alpha = 'a'; alpha <= 'z'; alpha++)
+	// The absolute difference between any 2 adjacent elements must be less than or equal to 1.
+	// Decrease the value of any element of arr to a smaller positive integer if it's not meet the above condition.
+	for (int i = 1; i < arr.size(); i++)
 	{
-		auto first = s.find(alpha);
-		auto last = s.find_last_of(alpha);
-		if (first < last)
-		{
-			// count character inside left and right, eg: aXa
-			string temp = s.substr(first + 1, last - first - 1);
-			vector<bool> found(26, 0);
-			for (auto ch : temp)
-				found[ch - 'a'] = true;
-
-			for (int i = 0; i < found.size(); i++)
-				if (found[i])
-					count++;
-		}
+		if (abs(arr[i] - arr[i - 1]) > 1)
+			arr[i] = arr[i - 1] + 1;
 	}
 
-	return count;
+	return arr.back();
 }
 
 int main() {

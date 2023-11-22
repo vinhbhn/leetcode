bcddc0bcf2173ab8868d92f6d39f2b00da9f1788
vinhbhn <bvinhhn@gmail.com>
2023-11-22 20:17:29 +0700

    easy: 705. design hashset

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,26 +17,51 @@
 
 using namespace std;
 
-int guessNumber(int n) {
-	int left = 1;
-	int right = n;
+// 96ms 184.65MB
+class MyHashSet {
+public:
+    vector<int> v;
 
-	while (left <= right)
-	{
-		int mid = left + (right - left) / 2;
-		int guessing = guess(mid);
+    MyHashSet() {
+        v.resize(1e6 + 1, 0);
+    }
 
-		if (guessing == 0)
-			return mid;
-		else if (guessing == -1)
-			right = mid - 1;
-		else
-			left = mid + 1;
+    void add(int key) {
+        v[key] = 1;
+    }
 
-	}
+    void remove(int key) {
+        v[key] = 0;
+    }
 
-	return -1;
-}}
+    bool contains(int key) {
+        return (v[key] == 1);
+    }
+};
+
+// 59ms 44MB
+class MyHashSet {
+private:
+    vector<bool> v;
+
+public:
+    MyHashSet() {
+        v.resize(1e6 + 1, false);
+    }
+
+    void add(int key) {
+        v[key] = true;
+    }
+
+    void remove(int key) {
+        v[key] = false;
+    }
+
+    bool contains(int key) {
+        return (v[key] == true);
+    }
+};
+
 
 int main() {
 	cout << boolalpha;

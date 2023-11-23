e09f0e0484ecce338fd6c6dabd01f8fdaf4a094a
vinhbhn <bvinhhn@gmail.com>
2023-11-23 16:43:31 +0700

    medium: 2414. Length of the Longest Alphabetical Continuous Substring

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,30 +17,102 @@
 
 using namespace std;
 
-// 4ms 7.7MB O(n^2) O(n)
-int numberOfArithmeticSlices(vector<int>& nums) {
-	int n = nums.size();
-	if (n < 3)
-		return 0;
-
-	int count = 0;
-
-	for (int i = 0; i < n - 2; i++)
+// 128ms 176.5MB
+int longestContinuousSubstring(string s) {
+	vector<string> v;
+	string temp = string(1, s[0]);
+	for (int i = 1; i < s.length(); i++)
 	{
-		int diff = nums[i + 1] - nums[i];
-		for (int j = i + 2; j < n; j++)
+		if (s[i] - 'a' - 1 == s[i - 1] - 'a')
+			temp += s[i];
+		else
 		{
-			// ex [1,2,3,4] if 3-2==diff (3 is arthmetic so 4-3==diff therefore 4 is arthmetic but not check all slice)
-			// bridging properties
-			if (nums[j] - nums[j - 1] == diff)
-				count++;
-			else
-				break;
+			v.push_back(temp); // maybe temp have s[i-1]
+
+			temp = s[i];
 		}
-		
+	}
+	if (temp != "")
+		v.push_back(temp);
+
+	int res = 0;
+	for (auto& x : v)
+	{
+		if (x.length() > res)
+			res = x.length();
 	}
 
-	return count;
+	return res; // max: 26
+}
+
+// 101ms 16.3MB due to remove duplicate string
+int longestContinuousSubstring(string s) {
+	unordered_set<string> st;
+	string temp = string(1, s[0]);
+	for (int i = 1; i < s.length(); i++)
+	{
+		if (s[i] - 'a' - 1 == s[i - 1] - 'a')
+			temp += s[i];
+		else
+		{
+			st.insert(temp); // maybe temp have one character is s[i-1]
+
+			temp = s[i]; // update temp to s[i], remove s[i-1]
+		}
+	}
+	if (temp != "")
+		st.insert(temp);
+
+	int res = 0;
+	for (auto& x : st)
+	{
+		if (x.length() > res)
+			res = x.length();
+	}
+
+	return res; // max: 26
+}
+
+// 62ms 15.7MB
+int longestContinuousSubstring(string s) {
+	string temp = string(1, s[0]);
+	int res = 0;
+	for (int i = 1; i < s.length(); i++)
+	{
+		if (s[i] - 'a' - 1 == s[i - 1] - 'a')
+			temp += s[i];
+		else
+		{
+			if (temp.length() > res)
+				res = temp.length();
+
+			temp = s[i]; // update temp to s[i], remove s[i-1]
+		}
+	}
+	if (temp != "" && temp.length() > res)
+		res = temp.length();
+
+	return res; // max: 26
+}
+
+// 41ms 15.9MB O(n) O(n)
+int longestContinuousSubstring(string s) {
+	int res = 0, count = 1; // because count always is 1 (s[i] == one character)
+	for (int i = 1; i < s.length(); i++)
+	{
+		if (s[i] - 1 == s[i - 1])
+			count++;
+		else
+		{
+			res = max(res, count);
+
+			count = 1; // because count always is 1 (s[i] == one character)
+		}
+	}
+	if (count > 0)
+		res = max(res, count);
+
+	return res; // max: 26
 }
 
 int main() {

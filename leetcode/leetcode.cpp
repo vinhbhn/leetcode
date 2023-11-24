8ba8cb68cc4b4c9e475f3d8bac9f2795943ce5c0
vinhbhn <bvinhhn@gmail.com>
2023-11-24 22:52:02 +0700

    easy: 2133. check if every row and column contains all numbers

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,25 +17,118 @@
 
 using namespace std;
 
-// 3ms 11.5MB 
-int diagonalSum(vector<vector<int>>& mat) {
-	int pri = 0, sec = 0;
-	int n = mat[0].size();
-	for (int row = 0, l = 0, r = n - 1; (row < n) && (l < n) && (r >= 0); row++, l++, r--)
-	{
-		// center will add only one element
-		// if and only if the length of the matrix is odd
-		if (n % 2 == 1 && l == r)
-		{
-			pri += mat[row][l];
-			continue;
-		}
+// 251ms 95.1MB
+bool checkValid(vector<vector<int>>& matrix) {
+	int n = matrix[0].size();
 
-		pri += mat[row][l];
-		sec += mat[row][r];
+	// check row
+	for (int row = 0; row < n; row++)
+	{
+		set<int> st;
+		for (int col = 0; col < n; col++)
+		{
+			st.insert(matrix[row][col]);
+		}
+		if (st.size() != n)
+			return false;
 	}
 
-	return pri + sec;
+	// check col
+	for (int col = 0; col < n; col++)
+	{
+		set<int> st;
+		for (int row = 0; row < n; row++)
+		{
+			st.insert(matrix[row][col]);
+		}
+		if (st.size() != n)
+			return false;
+	}
+
+	return true;
+}
+
+// 217ms 68.7MB
+bool checkValid(vector<vector<int>>& matrix) {
+	int n = matrix[0].size();
+	unordered_set<int> st;
+
+	// check row
+	for (int row = 0; row < n; row++)
+	{
+		st.clear();
+		for (int col = 0; col < n; col++)
+		{
+			st.insert(matrix[row][col]);
+		}
+		if (st.size() != n)
+			return false;
+	}
+
+	// check col
+	for (int col = 0; col < n; col++)
+	{
+		st.clear();
+		for (int row = 0; row < n; row++)
+		{
+			st.insert(matrix[row][col]);
+		}
+		if (st.size() != n)
+			return false;
+	}
+
+	return true;
+}
+
+// 83ms 35.7MB
+bool checkValid(vector<vector<int>>& matrix) {
+	int n = matrix[0].size();
+
+	for (int i = 0; i < n; i++)
+	{
+		vector<bool> r(n, false);
+		vector<bool> c(n, false);
+
+		// check row
+		for (int j = 0; j < n; j++)
+		{
+			r[matrix[i][j] - 1] = true;
+		}
+		if (count(r.begin(), r.end(), true) != n)
+			return false;
+
+		// check col
+		for (int j = 0; j < n; j++)
+		{
+			c[matrix[j][i] - 1] = true;
+		}
+		if (count(c.begin(), c.end(), true) != n)
+			return false;
+	}
+
+	return true;
+}
+
+// 100ms 35.7MB
+bool checkValid(vector<vector<int>>& matrix) {
+	int n = matrix[0].size();
+
+	for (int i = 0; i < n; i++)
+	{
+		vector<bool> rowCheck(n, false);
+		vector<bool> colCheck(n, false);
+
+		// check row
+		for (int j = 0; j < n; j++)
+		{
+			rowCheck[matrix[i][j] - 1] = true; // check row
+			colCheck[matrix[j][i] - 1] = true; // check col
+		}
+		if (count(rowCheck.begin(), rowCheck.end(), true) != n || count(colCheck.begin(), colCheck.end(), true) != n)
+			return false;
+	}
+
+	return true;
 }
 
 int main() {

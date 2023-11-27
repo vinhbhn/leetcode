a6c2f24151919d8cbd203845dbf5d6b187a57993
vinhbhn <bvinhhn@gmail.com>
2023-11-27 16:04:24 +0700

    medium: 935. Knight Dialer

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,27 +17,48 @@
 
 using namespace std;
 
-// 0ms 6.2MB
-int divisorSubstrings(int num, int k) {
-	string numStr = to_string(num);
+// https://leetcode.com/problems/knight-dialer/solutions/4333888/beats-100-explained-with-video-memorization-visualized-too/?envType=daily-question&envId=2023-11-27
+//41ms 8.5MB O(n*10) O(n*10)
+class Solution {
+public:
+    static const int mod = 1e9 + 7;
+    vector<vector<int>> MOVES = {
+    {4, 6},
+    {8, 6},
+    {7, 9},
+    {4, 8},
+    {3, 9, 0},
+    {},
+    {0, 1, 7},
+    {2, 6},
+    {1, 3},
+    {2, 4},
+    };
 
-	int i = 0, count = 0;
-	while (i + k <= numStr.length())
-	{
-		string temp = numStr.substr(i, k);
+    int cache[5001][10];
 
-		int numT = stoi(temp);
+    int knightDialer(int n) {
+        vector<int> nextNumbers = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        return knightDialer(n, nextNumbers);
+    }
 
-		if (numT != 0)
-			if (num % numT == 0)
-				count++;
+    int knightDialer(int remaining, vector<int>& nextNumbers) {
+        if (remaining == 1)
+            return nextNumbers.size();
 
-		i++;
-	}
-
-	return count;
-}
-
+        int count = 0;
+        for (int nextNumber : nextNumbers) {
+            int cur = cache[remaining][nextNumber];
+            if (cur == 0) {
+                cur = knightDialer(remaining - 1, MOVES[nextNumber]);
+                cache[remaining][nextNumber] = cur;
+            }
+            count += cur;
+            count %= mod;
+        }
+        return count;
+    }
+};
 
 int main() {
 	cout << boolalpha;

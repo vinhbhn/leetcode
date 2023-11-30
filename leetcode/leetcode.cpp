26bd1366333f566f6eeb3b360b87c7d53a7ae94a
vinhbhn <bvinhhn@gmail.com>
2023-11-30 16:33:18 +0700

    easy: 1108. defanging an IP address

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,90 +17,18 @@
 
 using namespace std;
 
-// 46/46 passed but take too long O(n^2) like brute force
-int hammingDistance(int x, int y) {
-	bitset<32> res(x ^ y);
-	return res.count();
-}
-int totalHammingDistance(vector<int>& nums) {
-	int count = 0;
-	for (int i = 0; i < nums.size() - 1; i++)
+// 0ms 6.5MB
+string defangIPaddr(string address) {
+	string res = "";
+	for (auto ch : address)
 	{
-		for (int j = i + 1; j < nums.size(); j++)
-		{
-			count += hammingDistance(nums[i], nums[j]);
-		}
+		if (ch == '.')
+			res += "[.]";
+		else
+			res += ch;
 	}
 
-	return count;
-}
-
-// https://leetcode.com/problems/total-hamming-distance/solutions/96243/share-my-o-n-c-bitwise-solution-with-thinking-process-and-explanation/
-// 48ms 19.4MB
-int totalHammingDistance(vector<int>& nums) {
-	int ans = 0, n = nums.size();
-	if (n < 2) return 0;
-	int zeroOne[2] = { 0, 0 }; // use stack (but no need to call delete function)
-	while (true)
-	{
-		int zeroCount = 0;
-		zeroOne[0] = 0;
-		zeroOne[1] = 0;
-		for (int i = 0; i < nums.size(); i++)
-		{
-			if (nums[i] == 0)
-				zeroCount++;
-			zeroOne[nums[i] % 2]++;
-			nums[i] >>= 1;
-		}
-		ans += zeroOne[0] * zeroOne[1];
-		if (zeroCount == nums.size())
-			return ans;
-	}
-
-	return 0;
-}
-// 57 and 61ms 19.3MB
-int totalHammingDistance(vector<int>& nums) {
-	int ans = 0, n = nums.size();
-	if (n < 2) return 0;
-	int* zeroOne = new int[2]; // use array dynamically
-	while (true)
-	{
-		int zeroCount = 0;
-		zeroOne[0] = 0;
-		zeroOne[1] = 0;
-		for (int i = 0; i < nums.size(); i++)
-		{
-			if (nums[i] == 0)
-				zeroCount++;
-			zeroOne[nums[i] % 2]++;
-			nums[i] >>= 1;
-		}
-		ans += zeroOne[0] * zeroOne[1];
-		if (zeroCount == nums.size())
-			return ans;
-	}
-
-	delete zeroOne;
-	return 0;
-}
-
-// https://leetcode.com/problems/total-hamming-distance/solutions/1585848/c-easy-and-concise-solutions/
-// 37ms 19.6MB O(n) O(1)
-int totalHammingDistance(vector<int>& nums) {
-	int n = nums.size(), ans = 0;
-	for (int i = 0; i < 32; i++)
-	{
-		int count = 0; // count of the no.of elements that have the 'i'th bitset
-		for (int k = 0; k < n; k++)
-		{
-			count += (nums[k] >> i) & 1;
-		}
-		ans += count * (n - count);
-	}
-
-	return ans;
+	return res;
 }
 
 int main() {

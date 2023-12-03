1d62fabbfd5cfddfba4ef623058503f212d50a92
vinhbhn <bvinhhn@gmail.com>
2023-12-03 15:51:14 +0700

    easy: 1652. Defuse the Bomb

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,96 +17,84 @@
 
 using namespace std;
 
-// 3ms 8MB
-bool isCircularSentence(string sentence) {
-	sentence += ' ';
-	string temp = "";
-	vector<string> v;
+// 0ms 8.9MB
+vector<int> decrypt(vector<int>& code, int k) {
+	int n = code.size();
+	vector<int> temp = code, res(n, 0);
 
-	for (auto ch : sentence)
+	// 5ms 8.8MB
+	/*temp.resize(2 * n);
+	for (int i = n; i < 2 * n; i++)
+		temp[i] = temp[i - n];*/
+		// replace by this
+	temp.insert(temp.end(), temp.begin(), temp.end());
+
+	if (k == 0)
+		return res;
+	else if (k > 0)
 	{
-		if (ch == ' ')
-		{
-			v.push_back(temp);
-			temp = "";
-		}
-		else
-			temp += ch;
+		for (int i = 0; i < n; i++)
+			res[i] = accumulate(temp.begin() + i + 1, temp.begin() + i + 1 + k, 0);
+	}
+	else if (k < 0)
+	{
+		for (int i = 0; i < n; i++)
+			res[i] = accumulate(temp.begin() + i + n + k, temp.begin() + i + n, 0);
 	}
 
-	char first = v[0][0];
-	char last = v[0][v[0].length() - 1];
-
-	for (int i = 1; i < v.size(); i++)
-	{
-		if (last != v[i][0])
-			return false;
-		else
-			last = v[i][v[i].length() - 1];
-	}
-
-	return (first == last); // last char in the last letter and first char in the first letter
+	return res;
 }
 
-// 3ms 8MB
-bool isCircularSentence(string sentence) {
-	sentence += ' ';
-	string temp = "";
-	vector<string> v;
-
-	for (auto ch : sentence)
-	{
-		if (ch == ' ')
-		{
-			v.push_back(temp);
-			temp = "";
-		}
-		else
-			temp += ch;
-	}
-
-	string curr = v[0];
-	char first = curr[0];
-	char last = curr[curr.length() - 1];
-
-	for (int i = 1; i < v.size(); i++)
-	{
-		curr = v[i];
-		if (last != curr[0]) // the last character of a word is equal to the first character of the next word.
-			return false;
-		else
-			last = curr[curr.length() - 1];
-
-	}
-
-	return (first == last); // the last character of the last word is equal to the first character of the first word.
-}
-
-// web 0ms real 0ms 7MB
-bool isCircularSentence(string sentence) {
-	int n = sentence.length();
-	if (sentence[0] != sentence[n - 1])
-		return false;
+// https://leetcode.com/problems/defuse-the-bomb/solutions/3367476/defuse-the-bomb-solution-in-c/
+// 0ms 8.8MB
+vector<int> decrypt(vector<int>& code, int k) {
+	int n = code.size();
+	vector<int> res(n, 0);
 	for (int i = 0; i < n; i++)
 	{
-		if (sentence[i] == ' ' && (sentence[i - 1] != sentence[i + 1]))
-			return false;
+		int sum = 0, m = abs(k), j = i;
+		while (m--)
+		{
+			if (k < 0)
+			{
+				j--;
+				if (j < 0)
+					j += n;
+				sum += code[j];
+			}
+			else if (k > 0)
+			{
+				j++;
+				if (j >= n)
+					j -= n;
+				sum += code[j];
+			}
+		}
+		res[i] = sum;
 	}
 
-	return true;
+	return res;
 }
 
 int main() {
 	cout << boolalpha;
-	string pattern = "abba", s = "dog cat cat dog";
-	cout << wordPattern(pattern, s) << '\n';
+	vector<int> code = { 5,7,1,4 };
+	int k = 3;
+	for (auto& x : decrypt(code, k))
+		cout << x << ' ';
+	cout << '\n';
 
-	pattern = "abba", s = "dog dog dog dog";
-	cout << wordPattern(pattern, s) << '\n';
+	code = { 5,7,1,4 };
+	k = 0;
+	for (auto& x : decrypt(code, k))
+		cout << x << ' ';
+	cout << '\n';
 
-	pattern = "aba", s = "cat cat cat dog";
-	cout << wordPattern(pattern, s) << '\n';
-
+	code = { 2,4,9,3 };
+	k = -2;
+	for (auto& x : decrypt(code, k))
+		cout << x << ' ';
+	cout << '\n';
 
 	return 0;
 }

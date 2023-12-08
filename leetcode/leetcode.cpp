78331aab18e1496cb6ad4c8a8156bd96f898e01f
vinhbhn <bvinhhn@gmail.com>
2023-12-08 21:46:55 +0700

    easy: 2824. count paris whose sum is less than target

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,16 +17,35 @@
 
 using namespace std;
 
-// 7ms 29.1MB
-vector<int> findWordsContaining(vector<string>& words, char x) {
-	vector<int> res;
-	for (int i = 0; i < words.size(); i++)
+// 7ms 20.7MB O(n^2) O(n)
+int countPairs(vector<int>& nums, int target) {
+	int count = 0;
+	for (int i = 0; i < nums.size() - 1; i++)
 	{
-		if (words[i].find(x) != string::npos)
-			res.push_back(i);
+		int t = target - nums[i];
+		for (int j = i + 1; j < nums.size(); j++)
+		{
+			if (nums[j] < t)
+				count++;
+		}
 	}
 
-	return res;
+	return count;
+}
+
+// 3ms 20.7MB
+int countPairs(vector<int>& nums, int target) {
+	int count = 0;
+	for (int i = 0; i < nums.size() - 1; i++)
+	{
+		for (int j = i + 1; j < nums.size(); j++)
+		{
+			if (nums[i] + nums[j] < target)
+				count++;
+		}
+	}
+
+	return count;
 }
 
 int main() {

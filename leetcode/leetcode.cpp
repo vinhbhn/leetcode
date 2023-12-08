635609ef9dd16f130677767afc7b67d849858f67
vinhbhn <bvinhhn@gmail.com>
2023-12-08 22:52:29 +0700

    easy: 2913. subarrays distinct element sum of squares I

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,35 +17,39 @@
 
 using namespace std;
 
-// 7ms 20.7MB O(n^2) O(n)
-int countPairs(vector<int>& nums, int target) {
-	int count = 0;
-	for (int i = 0; i < nums.size() - 1; i++)
+// 809ms 300MB
+int sumCounts(vector<int>& nums)
+{
+	int n = nums.size(), res = 0;
+	for (int step = 1; step <= n; step++)
 	{
-		int t = target - nums[i];
-		for (int j = i + 1; j < nums.size(); j++)
+		for (int i = 0, j = step; j <= n; i++, j++)
 		{
-			if (nums[j] < t)
-				count++;
+			unordered_set<int> st(nums.begin() + i, nums.begin() + j); // [first, last]
+			int sts = st.size();
+			res += sts * sts;
 		}
 	}
 
-	return count;
+	return res;
 }
 
-// 3ms 20.7MB
-int countPairs(vector<int>& nums, int target) {
-	int count = 0;
-	for (int i = 0; i < nums.size() - 1; i++)
+// web 27ms real 39ms 32.7MB
+int sumCounts(vector<int>& nums)
+{
+	int n = nums.size(), res = 0;
+	for (int i = 0; i < n; i++)
 	{
-		for (int j = i + 1; j < nums.size(); j++)
+		unordered_set<int> st;
+		for (int j = i; j < n; j++)
 		{
-			if (nums[i] + nums[j] < target)
-				count++;
+			st.insert(nums[j]);
+			int val = st.size();
+			res += val * val;
 		}
 	}
 
-	return count;
+	return res;
 }
 
 int main() {
@@ -59,24 +63,6 @@ static auto init = [](){
 	ios_base::sync_with_stdio(false);
 	cin.tie(nullptr);
 	cout.tie(nullptr);
-	return 0;
-}();
-*/
-/*
-int init = [] {
-	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	ofstream out("user.out");
-	cout.rdbuf(out.rdbuf());
-	for (string line; getline(cin, line); cout << '\n') {
-		istringstream ss(line);
-		char ch;
-		int a, b, c, d;
-		ss >> ch >> ch >> a >> ch >> b >> ch >> ch >> ch >> c >> ch >> d;
-		if (a == c || a == d) cout << a;
-		else cout << b;
-	}
-	exit(0);
 	return 0;
 }();
 */

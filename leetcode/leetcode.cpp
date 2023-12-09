bd538c53215d9a3af3b835e76e6accf4a346e80f
vinhbhn <bvinhhn@gmail.com>
2023-12-09 22:22:31 +0700

    medium: 848. shifting letters

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,19 +17,80 @@
 
 using namespace std;
 
-// 0ms 6.6MB
-string replaceDigits(string s) {
-	for (int i = 1; i < s.size(); i += 2)
+// 44/57 left to right
+//char shift(char ch, int num)
+//{
+//	num %= 26; // if num > 26
+//
+//	if (ch + num > 'z')
+//	{
+//		num -= 'z' - ch;
+//		num--; // wrap
+//		ch = 'a';
+//	}
+//
+//	return (ch + num);
+//}
+//string shiftingLetters(string s, vector<int>& shifts) {
+//	for (int i = 0; i < shifts.size(); i++)
+//	{
+//		// if shifts[i] > 26
+//		shifts[i] %= 26;
+//		for (int j = 0; j <= i; j++)
+//		{
+//			s[j] = shift(s[j], shifts[i]);
+//		}
+//	}
+//
+//	return s;
+//}
+
+// 101ms 68.3MB
+char shift(char ch, int num)
+{
+	if (ch + num > 'z')
 	{
-		s[i] = s[i - 1] + (s[i] - '0'); 
+		num -= 'z' - ch;
+		num--; // wrap
+		ch = 'a';
+	}
+
+	return (ch + num);
+}
+string shiftingLetters(string s, vector<int>& shifts) {
+	int sum = 0;
+	for (int i = shifts.size() - 1; i >= 0; i--)
+	{
+		// if sum > 26
+		sum += shifts[i];
+		sum %= 26;
+		s[i] = shift(s[i], sum);
 	}
 
 	return s;
 }
 
+// web
+char shift(char ch, int num)
+{
+	return (ch - 'a' + num) % 26 + 'a';
+}
+
 int main() {
 	cout << boolalpha;
-	cout << replaceDigits("a1c1e1") << '\n';
+	cout << shift('a', 1) << '\n';
+	cout << shift('z', 1) << '\n';
+	cout << shift('y', 1) << '\n';
+	cout << shift('y', 2) << '\n';
+	cout << shift('y', 3) << '\n';
+	cout << shift('y', 4) << '\n';
+	vector<int> shifts = { 3,5,9 };
+	cout << shiftingLetters("abc", shifts) << '\n';
+	shifts = { 52 };
+	cout << shiftingLetters("z", shifts) << '\n';
+
+	cout << shift('z', 52) << '\n'; // curr not work with z because num % 26
+	cout << shift('b', 14) << '\n';
 
 	return 0;
 }

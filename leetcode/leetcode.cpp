c55ae829ad7942a507427af526ee959d53f1450e
vinhbhn <bvinhhn@gmail.com>
2023-12-19 21:11:18 +0700

    easy: 1913. maximum product difference between two pairs

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,54 +17,16 @@
 
 using namespace std;
 
-// 4ms 7.9MB O(s+t) O(vs+vt)
-bool isAnagram(string s, string t) {
-	if (s.size() != t.size())
-		return false;
+// 27ms 20.6MB O(nlogn) O(n)
+int maxProductDifference(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
 
-	vector<int> vs(26, 0), vt(26, 0);
-	for (int i = 0; i < s.size(); i++)
-	{
-		vs[s[i] - 'a']++;
-		vt[t[i] - 'a']++;
-	}
-
-	for (int i = 0; i < 26; i++)
-	{
-		if (vs[i] != vt[i]) // letter in s not present/equal quantity in t
-			return false;
-	}
-
-	return true;
+	int n = nums.size();
+	return (nums[n - 1] * nums[n - 2]) - (nums[0] * nums[1]); // (a*b) - (c*d)
 }
-
-// web 0ms real 3ms 7.8MB
-bool isAnagram(string s, string t) {
-	if (s.size() != t.size())
-		return false;
-
-	vector<int> count(26, 0);
-	for (int i = 0; i < s.size(); i++)
-	{
-		count[s[i] - 'a']++;
-		count[t[i] - 'a']--;
-	}
-
-	for (int i = 0; i < 26; i++)
-	{
-		if (count[i] != 0) // letter in s not present/equal quantity in t
-			return false;
-	}
-
-	return true;
-}
-
-// sorting (s) == sorting (t)
 
 int main() {
 	cout << boolalpha;
-	string  s = "anagram", t = "nagaram";
-	cout << isAnagram(s, t) << '\n';
 
 
 	return 0;

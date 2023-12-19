426d02673bdd9b4bb2ed32684d402694fa6e19b2
vinhbhn <bvinhhn@gmail.com>
2023-12-19 21:32:06 +0700

    easyt: 2574. left and right sum differences

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,12 +17,61 @@
 
 using namespace std;
 
-// 27ms 20.6MB O(nlogn) O(n)
-int maxProductDifference(vector<int>& nums) {
-	sort(nums.begin(), nums.end());
-
+// 7ms 12MB O(2n + n/2) O(2n)
+vector<int> leftRightDifference(vector<int>& nums) {
 	int n = nums.size();
-	return (nums[n - 1] * nums[n - 2]) - (nums[0] * nums[1]); // (a*b) - (c*d)
+	if (n == 1)
+		return { 0 };
+
+	int left = 0, right = 0;
+	vector<int> l = { 0 }, r = { 0 };
+	for (int i = 0; i < n - 1; i++)
+	{
+		left += nums[i];
+		l.push_back(left);
+	}
+	for (int i = n - 1; i >= 1; i--)
+	{
+		right += nums[i];
+		r.push_back(right);
+	}
+	reverse(r.begin(), r.end());
+
+	for (int i = 0; i < n; i++)
+	{
+		nums[i] = abs(l[i] - r[i]);
+	}
+
+	return nums;
+}
+
+// 6ms 11.4MB 
+vector<int> leftRightDifference(vector<int>& nums) {
+	int left = 0, right = accumulate(nums.begin(), nums.end(), 0), temp = 0;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		temp = nums[i];
+		right -= temp;
+		nums[i] = abs(left - right);
+		left += temp;
+
+	}
+
+	return nums;
+}
+
+//web 0ms real 10ms 11.3MB O(n) O(n)
+vector<int> leftRightDifference(vector<int>& nums) {
+	int left = 0, right = accumulate(nums.begin(), nums.end(), 0);
+	vector<int> res;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		right -= nums[i];
+		res.push_back(abs(left - right));
+		left += nums[i];
+	}
+
+	return res;
 }
 
 int main() {

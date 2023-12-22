0ac4347b5c280df88d16c142933c04243a842b93
vinhbhn <bvinhhn@gmail.com>
2023-12-22 21:28:39 +0700

    easy: 2427. number of common factors

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,54 +17,25 @@
 
 using namespace std;
 
-// 38ms 15.3MB O(n) O(n/2)
-string makeSmallestPalindrome(string s) {
-	int n = s.length(), i = 0;
-
-	while (true)
+// 0ms 6.2MB
+int commonFactors(int a, int b) {
+	int count = 0;
+	// count divisible by both A and B from 1 to 1000
+	for (int i = 1; i <= 1000; i++)
 	{
-		if (i >= n - 1 - i)
-			break;
-
-		if (s[i] > s[n - 1 - i])
-			s[i] = s[n - 1 - i];
-		else if (s[i] < s[n - 1 - i])
-			s[n - 1 - i] = s[i];
-
-		i++;
+		if ((a % i == 0) && (b % i == 0))
+			count++;
 	}
 
-	return s;
-}
+	// duplicate because from 1 to 1000 have a and b
+	if (a == b)
+		count--;
 
-// web 19ms real 31ms 15.3MB
-string makeSmallestPalindrome(string s) {
-	int l = 0, r = s.length() - 1;
-
-	while (l <= r)
-	{
-		if (s[l] > s[r])
-			s[l] = s[r];
-		else if (s[l] < s[r])
-			s[r] = s[l];
-		l++;
-		r--;
-	}
-
-	return s;
+	return (b % a == 0) ? count + 1 : count;
 }
 
 int main() {
 	cout << boolalpha;
-	string s = "egcfe";
-	cout << makeSmallestPalindrome(s) << '\n';
-
-	s = "abcd";
-	cout << makeSmallestPalindrome(s) << '\n';
-
-	s = "seven";
-	cout << makeSmallestPalindrome(s) << '\n';
-
 
 	return 0;
 }

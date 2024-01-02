72b14059dae8c0c53b20556e686e813d2232672c
vinhbhn <bvinhhn@gmail.com>
2024-01-02 21:20:54 +0700

    easy: 2974. minimum number game

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,83 +17,28 @@
 
 using namespace std;
 
-// 13ms 22.2MB O(m+n+k) O(m+n+k) k = check.size()
-int minNumber(vector<int>& nums1, vector<int>& nums2) {
-	int m1 = 9, m2 = 9;
+// 22ms 24.4MB O(nlogn + n/2) O(n)
+vector<int> numberGame(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
 
-	// find one digit exists in both array
-	vector<int> check(10, 0);
-	for (auto num : nums1)
+	vector<int> res;
+	for (int i = 0; i < nums.size() - 1; i += 2)
 	{
-		check[num]++;
-		m1 = min(m1, num);
-	}
-	for (auto num : nums2)
-	{
-		check[num]++;
-		m2 = min(m2, num);
-	}
-	for (int i = 1; i < 10; i++)
-	{
-		if (check[i] >= 2)
-			return i;
+		res.push_back(nums[i + 1]); // bob remove second minimum then append first
+		res.push_back(nums[i]); // alice remove first minimum then append after Bob
 	}
 
-	// find min digit in nums1 and nums2 then merge them
-	if (m1 > m2)
-		swap(m1, m2);
-
-	return m1 * 10 + m2; // smallest number
+	return res;
 }
 
-// 3ms 22.1MB O(mlogm+nlogn) O(m+n)
-int minNumber(vector<int>& nums1, vector<int>& nums2) {
-	sort(nums1.begin(), nums1.end());
-	sort(nums2.begin(), nums2.end());
+// 23ms 24.4MB O(nlogn + n/2) O(n)
+vector<int> numberGame(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
 
-	// find one digit exists in both array
-	vector<int> check(10, 0);
-	for (auto num : nums1)
-	{
-		check[num]++;
-	}
-	for (auto num : nums2)
-	{
-		check[num]++;
-		if (check[num] == 2)
-			return num;
-	}
+	for (int i = 0; i < nums.size() - 1; i += 2)
+		swap(nums[i], nums[i + 1]);
 
-	// find min digit in nums1 and nums2 then merge them
-	int m1 = nums1[0], m2 = nums2[0];
-	if (m1 > m2)
-		swap(m1, m2);
-
-	return m1 * 10 + m2; // smallest number
-}
-
-int minNumber(vector<int>& nums1, vector<int>& nums2) {
-	sort(nums1.begin(), nums1.end());
-	sort(nums2.begin(), nums2.end());
-
-	// find one digit exists in both array
-	vector<int> check(10, 0);
-	for (auto num : nums1)
-	{
-		check[num]++;
-	}
-	for (auto num : nums2)
-	{
-		if (++check[num] == 2)
-			return num;
-	}
-
-	// find min digit in nums1 and nums2 then merge them
-	int m1 = nums1[0], m2 = nums2[0];
-	if (m1 > m2)
-		swap(m1, m2);
-
-	return m1 * 10 + m2; // smallest number
+	return nums;
 }
 
 int main() {

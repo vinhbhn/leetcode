0fc5447b1e72825fd431b0398acffb25e181aaea
vinhbhn <bvinhhn@gmail.com>
2024-01-03 20:56:05 +0700

    medium: 2125. number of laser beams in a bank

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,17 +17,58 @@
 
 using namespace std;
 
-// 4ms 8.4MB O(n) O(n)
-int largestAltitude(vector<int>& gain) {
-	int highest = 0, n = gain.size(), curr = 0;
+// 81ms 29.4MB O(b*b[0])
+int countLaser(string s)
+{
+	int count = 0;
+	for (auto& ch : s)
+		if (ch == '1')
+			count++;
 
-	for (int i = 0; i < n; i++)
+	return count;
+}
+int numberOfBeams(vector<string>& bank) {
+	vector<int> nums;
+	int temp = 0;
+	for (int i = 0; i < bank.size(); i++)
 	{
-		curr += gain[i];
-		highest = max(highest, curr);
+		temp = countLaser(bank[i]);
+		if (temp != 0)
+			nums.push_back(temp);
 	}
 
-	return highest;
+	int res = 0;
+	for (int i = 1; i < nums.size(); i++)
+	{
+		res += nums[i] * nums[i - 1];
+	}
+
+	return res;
+}
+
+// 83ms 29.1MB O(b*b[0])
+int countLaser(string s)
+{
+	int count = 0;
+	for (auto& ch : s)
+		if (ch == '1')
+			count++;
+
+	return count;
+}
+int numberOfBeams(vector<string>& bank) {
+	int temp = 0, res = 0, lastValue = 0;
+	for (int i = 0; i < bank.size(); i++)
+	{
+		temp = countLaser(bank[i]);
+		if (temp != 0)
+		{
+			res += lastValue * temp;
+			lastValue = temp;
+		}
+	}
+
+	return res;
 }
 
 int main() {

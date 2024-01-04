e29bc0a92fec9085066016eabd4a3359ca1d718e
vinhbhn <bvinhhn@gmail.com>
2024-01-04 22:00:28 +0700

    easy: 1637. widest vertical area between two points containing no points

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,55 +17,20 @@
 
 using namespace std;
 
-// 81ms 29.4MB O(b*b[0])
-int countLaser(string s)
-{
-	int count = 0;
-	for (auto& ch : s)
-		if (ch == '1')
-			count++;
-
-	return count;
-}
-int numberOfBeams(vector<string>& bank) {
-	vector<int> nums;
-	int temp = 0;
-	for (int i = 0; i < bank.size(); i++)
+// 168ms 68.7MB O(nlogn)
+int maxWidthOfVerticalArea(vector<vector<int>>& points) {
+	vector<int> x;
+	for (int i = 0; i < points.size(); i++)
 	{
-		temp = countLaser(bank[i]);
-		if (temp != 0)
-			nums.push_back(temp);
+		x.push_back(points[i][0]);
 	}
+
+	sort(x.begin(), x.end());
 
 	int res = 0;
-	for (int i = 1; i < nums.size(); i++)
+	for (int i = 1; i < x.size(); i++)
 	{
-		res += nums[i] * nums[i - 1];
-	}
-
-	return res;
-}
-
-// 83ms 29.1MB O(b*b[0])
-int countLaser(string s)
-{
-	int count = 0;
-	for (auto& ch : s)
-		if (ch == '1')
-			count++;
-
-	return count;
-}
-int numberOfBeams(vector<string>& bank) {
-	int temp = 0, res = 0, lastValue = 0;
-	for (int i = 0; i < bank.size(); i++)
-	{
-		temp = countLaser(bank[i]);
-		if (temp != 0)
-		{
-			res += lastValue * temp;
-			lastValue = temp;
-		}
+		res = max(res, x[i] - x[i - 1]);
 	}
 
 	return res;

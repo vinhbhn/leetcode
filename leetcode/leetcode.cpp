718d5756b87612d9dab338345d9c75e54031ccc9
vinhbhn <bvinhhn@gmail.com>
2024-01-04 22:23:51 +0700

    easy: 2656. maximum sum with exactly k elements

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,16 +17,39 @@
 
 using namespace std;
 
-// 0ms 20.7MB O(n)
-int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
-	int count = 0;
-	for (auto hour : hours)
+// 36ms 71MB O(n+k)
+int maximizeSum(vector<int>& nums, int k) {
+	int largestNum = *max_element(nums.begin(), nums.end());
+	int sum = largestNum;
+
+	k--; // after init sum = largest element in num
+	while (k)
 	{
-		if (hour >= target)
-			count++;
+		largestNum++;
+		sum += largestNum;
+
+		k--;
 	}
 
-	return count;
+	return sum;
+}
+
+// https://leetcode.com/problems/maximum-sum-with-exactly-k-elements/solutions/3466875/very-simple-easy-to-understand-solution/
+// 34ms 71.1MB O(n+k)
+int sumUptoN(int n)
+{
+	return (n * (n + 1) / 2);
+}
+int maximizeSum(vector<int>& nums, int k) {
+	int largestNum = *max_element(nums.begin(), nums.end());
+	return sumUptoN(largestNum+k-1) - sumUptoN(largestNum-1);
+}
+
+// web 4ms real 16ms 71.1MB O(n)
+int maximizeSum(vector<int>& nums, int k) {
+	int largestNum = *max_element(nums.begin(), nums.end());
+	double k_over_2 = k / 2.0;
+	return k * largestNum + k_over_2 * (k+1) - k;
 }
 
 int main() {

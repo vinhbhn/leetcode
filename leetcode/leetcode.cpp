d802a55ebb339452c5fa1ad2bdd129ff45d33b5f
vinhbhn <bvinhhn@gmail.com>
2024-01-08 21:51:26 +0700

    easy: 1614. maximum nesting depth of the parentheses

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,61 +17,20 @@
 
 using namespace std;
 
-// 10ms 21.2MB O(n^2)
-int maximumNumberOfStringPairs(vector<string>& words) {
-	int count = 0;
-	unordered_set<int> st; // add known pair, each string can belong in at most one pair
-	for (int i = 0; i < words.size() - 1; i++)
+// 0ms 6.5MB O(n)
+int maxDepth(string s) {
+	int count = 0, maxCount = 0;
+	for (auto ch : s)
 	{
-		for (int j = i + 1; j < words.size(); j++)
-		{
-			if (!st.contains(i) and !st.contains(j))
-			{
-				if ((words[i][0] == words[j][1]) and (words[i][1] == words[j][0]))
-				{
-					count++;
-					st.insert(i);
-					st.insert(j);
-				}
-			}
-		}
-	}
-
-	return count;
-}
-
-// web 0ms real 17ms 21.6MB 
-int maximumNumberOfStringPairs(vector<string>& words) {
-	int count = 0;
-	unordered_set<string> st; // add known string, each string can belong in at most one pair
-	for (int i = 0; i < words.size(); i++)
-	{
-		string temp = words[i];
-		reverse(temp.begin(), temp.end());
-		if (st.contains(temp))
+		if (ch == '(')
 			count++;
-		else
-			st.insert(words[i]);
+		else if (ch == ')')
+			count--;
+
+		maxCount = max(maxCount, count);
 	}
 
-	return count;
-}
-
-// web 0ms real 0ms 21.7MB // st.contain vs st.find
-int maximumNumberOfStringPairs(vector<string>& words) {
-	int count = 0;
-	unordered_set<string> st; // add known string, each string can belong in at most one pair
-	for (int i = 0; i < words.size(); i++)
-	{
-		string temp = words[i];
-		reverse(temp.begin(), temp.end());
-		if (st.find(temp) != st.end())
-			count++;
-		else
-			st.insert(words[i]);
-	}
-
-	return count;
+	return maxCount;
 }
 
 int main() {

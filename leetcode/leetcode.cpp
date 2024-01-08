bbb76923607a89bccbf6edef65e8b59b2d420b4e
vinhbhn <bvinhhn@gmail.com>
2024-01-08 22:36:50 +0700

    easy: 2441. largest positive integer that exists with its negative

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,33 +17,44 @@
 
 using namespace std;
 
-// 43ms 23.5MB O(words.size() * s.size())
-bool isPalindrome(string s)
-{
-	return equal(s.cbegin(), s.cbegin() + s.size() / 2, s.crbegin());
+// 27ms 22.9MB
+int findMaxK(vector<int>& nums) {
+	sort(nums.begin(), nums.end());
+	unordered_map<int, int> mp;
+
+	int res = -1;
+
+	// sort value from low to high
+	// append negative number with key = abs(negative number), value = negative number
+	for (int num : nums)
+	{
+		if (mp.contains(num) and mp[num] == -num)
+			res = max(res, num);
+		else
+			mp[abs(num)] = num;
+	}
+
+	return res;
 }
-string firstPalindrome(vector<string>& words) {
-	for (auto& word : words)
-		if (isPalindrome(word))
-			return word;
 
-	return "";
-}
+// web 3ms real 18ms 20.1MB
+int findMaxK(vector<int>& nums) {
+	vector<bool> negative(1001, false);
+	vector<bool> positive(1001, false);
+	for (int i = 0; i < nums.size(); i++)
+	{
+		int x = nums[i];
+		if (x < 0)
+			negative[abs(x)] = true;
+		else
+			positive[x] = true;
+	}
 
-// 39ms 23.3MB
-string firstPalindrome(vector<string>& words) {
-	for (auto& word : words)
-		if (isPalindrome(word))
-			return word;
+	for (int i = 1000; i > 0; i--)
+		if (negative[i] and positive[i])
+			return i;
 
-	return "";
-}
-string firstPalindrome(vector<string>& words) {
-	for (auto& word : words)
-		if (isPalindrome(word))
-			return word;
-
-	return "";
+	return -1;
 }
 
 int main() {

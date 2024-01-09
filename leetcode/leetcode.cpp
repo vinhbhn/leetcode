48321f9848d074fc93797e25bb6860e9530c39c2
vinhbhn <bvinhhn@gmail.com>
2024-01-09 20:52:07 +0700

    easy: 1827. minimum operations to make the array increasing

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,44 +17,19 @@
 
 using namespace std;
 
-// 27ms 22.9MB
-int findMaxK(vector<int>& nums) {
-	sort(nums.begin(), nums.end());
-	unordered_map<int, int> mp;
-
-	int res = -1;
-
-	// sort value from low to high
-	// append negative number with key = abs(negative number), value = negative number
-	for (int num : nums)
+// 17ms 16.1MB
+int minOperations(vector<int>& nums) {
+	int count = 0;
+	for (int i = 1; i < nums.size(); i++)
 	{
-		if (mp.contains(num) and mp[num] == -num)
-			res = max(res, num);
-		else
-			mp[abs(num)] = num;
+		if (nums[i] <= nums[i - 1])
+		{
+			count += nums[i - 1] + 1 - nums[i];
+			nums[i] = nums[i - 1] + 1;
+		}
 	}
 
-	return res;
-}
-
-// web 3ms real 18ms 20.1MB
-int findMaxK(vector<int>& nums) {
-	vector<bool> negative(1001, false);
-	vector<bool> positive(1001, false);
-	for (int i = 0; i < nums.size(); i++)
-	{
-		int x = nums[i];
-		if (x < 0)
-			negative[abs(x)] = true;
-		else
-			positive[x] = true;
-	}
-
-	for (int i = 1000; i > 0; i--)
-		if (negative[i] and positive[i])
-			return i;
-
-	return -1;
+	return count;
 }
 
 int main() {

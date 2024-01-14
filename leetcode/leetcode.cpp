d874d9a9f64c869c4cf31fdb986c7ccfe61a7dfb
vinhbhn <bvinhhn@gmail.com>
2024-01-14 15:36:39 +0700

    easy: 1704. determine if string halves are alike

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,97 +17,25 @@
 
 using namespace std;
 
-class ParkingSystem {
-private:
-	int m_big{};
-	int m_medium{};
-	int m_small{};
-public:
-	ParkingSystem(int big = 0, int medium = 0, int small = 0) : m_big{ big }, m_medium{ medium }, m_small{ small } {
+// 0ms 7.1MB O(n)
+bool isVowel(char ch)
+{
+	return (ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u' or ch == 'A' or ch == 'E' or ch == 'I' or ch == 'O' or ch == 'U');
+}
+bool halvesAreAlike(string s) {
+	int n = s.length(), half = n / 2, countA = 0, countB = 0;
+
+	for (int i = 0; i < half; i++)
+	{
+		if (isVowel(s[i]))
+			countA++;
+
+		if (isVowel(s[i + half]))
+			countB++;
 	}
 
-	bool addCar(int carType) {
-		if (carType == 1)
-		{
-			if (m_big > 0)
-			{
-				m_big -= 1;
-				return true;
-			}
-		}
-		else if (carType == 2)
-		{
-			if (m_medium > 0)
-			{
-				m_medium -= 1;
-				return true;
-			}
-		}
-		else if (carType == 3)
-		{
-			if (m_small > 0)
-			{
-				m_small -= 1;
-				return true;
-			}
-		}
-
-		return false;
-	}
-};
-
-// 52ms 33.5MB
-class ParkingSystem {
-private:
-	int m_big{};
-	int m_medium{};
-	int m_small{};
-public:
-	ParkingSystem(int big = 0, int medium = 0, int small = 0) : m_big{ big }, m_medium{ medium }, m_small{ small } {
-	}
-
-	bool addCar(int carType) {
-		if (carType == 1 and m_big > 0)
-		{
-			m_big -= 1;
-			return true;
-		}
-		else if (carType == 2 and m_medium > 0)
-		{
-			m_medium -= 1;
-			return true;
-		}
-		else if (carType == 3 and m_small > 0)
-		{
-			m_small -= 1;
-			return true;
-		}
-
-		return false;
-	}
-};
-
-// web 24ms real 56ms 33.4MB
-class ParkingSystem {
-private:
-	int m_parking[3] = { 0 };
-public:
-	ParkingSystem(int big = 0, int medium = 0, int small = 0) {
-		m_parking[0] = big;
-		m_parking[1] = medium;
-		m_parking[2] = small;
-	}
-
-	bool addCar(int carType) {
-		if (m_parking[carType - 1])
-		{
-			--m_parking[carType - 1];
-			return true;
-		}
-
-		return false;
-	}
-};
+	return (countA == countB);
+}
 
 int main() {
 	cout << boolalpha;

245178b1dd69cdfbe8e96c7b08b2225f4d951640
vinhbhn <bvinhhn@gmail.com>
2024-01-17 20:20:59 +0700

    easy: 2651. calculate delayed arrival time

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,32 +17,9 @@
 
 using namespace std;
 
-// 11ms 9.3MB
-string removeTrailingZeros(string num) {
-	int count = 0;
-	for (int i = num.length() - 1; i >= 0; i--)
-	{
-		if (num[i] == '0')
-			count++;
-		else
-			break;
-	}
-
-	return num.substr(0, num.length() - count);
-}
-
-// web 0ms real 12ms 9.7MB
-string removeTrailingZeros(string num) {
-	int j = num.length() - 1;
-	while (num[j] == '0')
-		j--;
-
-	string res = "";
-	for (int i = 0; i <= j; i++)
-	{
-		res += num[i];
-	}
-	return res;
+// 0ms 6.2MB
+int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+	return (arrivalTime + delayedTime) % 24;
 }
 
 int main() {

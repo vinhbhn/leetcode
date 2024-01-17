9501cfc5eb66cb0fe5a214e7b5c08635d477286c
vinhbhn <bvinhhn@gmail.com>
2024-01-17 20:40:32 +0700

    easy: 2373. largest local values in a matrix

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,9 +17,34 @@
 
 using namespace std;
 
-// 0ms 6.2MB
-int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
-	return (arrivalTime + delayedTime) % 24;
+// 11ms 12.3MB
+int maxValueInMatrix3x3(vector<vector<int>>& grid, int row, int col)
+{
+	int val = 0;
+	for (int i = row; i < row + 3; i++)
+	{
+		for (int j = col; j < col + 3; j++)
+		{
+			val = max(val, grid[i][j]);
+		}
+	}
+
+	return val;
+}
+vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+	vector<vector<int>> res;
+
+	for (int i = 0; i < grid.size() - 2; i++)
+	{
+		vector<int> t;
+		for (int j = 0; j < grid[0].size() - 2; j++)
+		{
+			t.push_back(maxValueInMatrix3x3(grid, i, j));
+		}
+		res.push_back(t);
+	}
+
+	return res;
 }
 
 int main() {

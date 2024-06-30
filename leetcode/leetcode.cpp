b6879d368c5c59db8beae3f7226068cd066392c0
vinhbhn <bvinhhn@gmail.com>
2024-06-30 17:11:50 +0700

    easy: 1869. longer contiguous segments of ones than zeros

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,29 +17,72 @@
 
 using namespace std;
 
-int absASCII(char a, char b)
-{
-	return abs(a - b);
-}
+// O(n) O(1)
+bool checkZeroOnes(string s) {
+	int l1 = 0, l0 = 0;
+	int c1 = 0, c0 = 0;
 
-int scoreOfString(string s) {
-	int res = 0;
-	
-	for (int i = 0; i < s.size() - 1; i++)
+	// if s size is 1
+	if (s.size() == 1)
 	{
-		res += absASCII(s[i], s[i + 1]);
+		return (s[0] == '1');
 	}
 
-	return res;
-}
+	for (int i = 0; i < s.size() - 1; i++)
+	{
+		// before: s[i], after: s[i+1]
+		// after == before: "before + 1"
+		// after != before: "before + 1" then save "before" and compare "before" to "longest before"
+		// problem: not count last digit of s
 
+		if (s[i + 1] == '1')
+		{
+			if (s[i] == '1')
+				c1++;
+			else
+			{
+				c0++;
+				l0 = max(l0, c0);
+				c0 = 0;
+			}
+		}
+
+		if (s[i + 1] == '0')
+		{
+			if (s[i] == '0')
+				c0++;
+			else
+			{
+				c1++;
+				l1 = max(l1, c1);
+				c1 = 0;
+			}
+		}
+	}
+
+	// count last digit of s
+	if (s[s.size() - 1] == '1')
+	{
+		c1++;
+		l1 = max(l1, c1);
+	}
+	else
+	{
+		c0++;
+		l0 = max(l0, c0);
+	}
+
+	return (l1 > l0);
+}
 
 int main() {
 	cout << boolalpha;
 
-	cout << scoreOfString("hello") << '\n';
-	cout << scoreOfString("zaz") << '\n';
-
+	cout << checkZeroOnes("1101") << '\n';
+	cout << checkZeroOnes("111000") << '\n';
+	cout << checkZeroOnes("110100010") << '\n';
+	cout << checkZeroOnes("1") << '\n';
+	cout << checkZeroOnes("11111111111111111111111111111111111111111111111111") << '\n';
 
 	return 0;
 }

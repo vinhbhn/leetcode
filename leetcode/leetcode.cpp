f6d3f2578df5ae9453751ca858d542e28654ea81
vinhbhn <bvinhhn@gmail.com>
2024-07-01 21:06:05 +0700

    easy: 3019. number of changing keys

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,44 +17,30 @@
 
 using namespace std;
 
-// 6ms 8.7MB O(n) O(1)
-string maximumOddBinaryNumber(string s) {
-	int c0 = 0, c1 = 0;
 
-	for (int i = 0; i < s.size(); i++)
+// O(n) O(1) 3ms 7.8MB
+int countKeyChanges(string s) {
+	transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+	int count = 0;
+	for (int i = 0; i < s.size() - 1; i++)
 	{
-		if (s[i] == '0')
-			c0++;
-		else
-			c1++;
+		if (s[i] != s[i + 1])
+		{
+			count++;
+		}
 	}
 
-	string res = "";
-	// if c1 >= 2, then fill up with "1" until c1 equal 1
-	// fill up res with "0" until c0 equal 0
-	// fill "1" end of res
-	while (c1 > 1)
-	{
-		res += "1";
-		c1--;
-	}
-	while (c0)
-	{
-		res += "0";
-		c0--;
-	}
-	res += "1";
-
-	return res;
+	return count;
 }
 
 int main() {
 	cout << boolalpha;
 	
-	cout << maximumOddBinaryNumber("010") << '\n';
-	cout << maximumOddBinaryNumber("0101") << '\n';
-	cout << maximumOddBinaryNumber("1") << '\n';
-	cout << maximumOddBinaryNumber("0111") << '\n';
+	cout << countKeyChanges("aAbBcC") << '\n';
+	cout << countKeyChanges("AaAaAaaA") << '\n';
+	cout << countKeyChanges("AaAaAaaAb") << '\n';
+
 
 
 	return 0;

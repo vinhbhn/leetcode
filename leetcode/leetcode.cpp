4e9060ff59790fcfe589860b71d9a765888100cf
vinhbhn <bvinhhn@gmail.com>
2024-07-03 15:58:30 +0700

    easy: 1021. remove outermost parentheses

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,14 +17,37 @@
 
 using namespace std;
 
-// O(n) 3ms 8.3MB
-int findPermutationDifference(string s, string t) {
-	int res = 0;
+// O(n) 2ms 7.8MB
+string removeOuterParentheses(string s) {
+	string res = "";
 
-	for (int i = 0; i < s.size(); i++)
+	// count (: c1
+	// count ): c2
+	int c1 = 0, c2 = 0;
+
+	for (auto ch : s)
 	{
-		// permutation difference between index 
-		res += abs(static_cast<int>(t.find(s[i])) - i);
+		if (ch == '(')
+		{
+			c1++;
+			// outer appear
+			if (c1 > c2 + 1)
+			{
+				res += "(";
+			}
+		}
+
+		if (ch == ')')
+		{
+			c2++;
+			// c1 == c2: outer remove
+			if (c1 > c2)
+			{
+				res += ")";
+				c2--;
+				c1--;
+			} 
+		}
 	}
 
 	return res;
@@ -33,6 +56,10 @@ int findPermutationDifference(string s, string t) {
 
 int main() {
 	cout << boolalpha;
+
+	cout << removeOuterParentheses("(()())(())") << '\n';
+	cout << removeOuterParentheses("(()())(())(()(()))") << '\n';
+	cout << removeOuterParentheses("()()") << '\n';
 
 
 	return 0;

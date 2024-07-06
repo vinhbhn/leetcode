1d28dd0857145f149a4df8bc9135b5d17add4e1d
vinhbhn <bvinhhn@gmail.com>
2024-07-06 23:00:52 +0700

    add more solution by learn

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -18,27 +18,34 @@
 using namespace std;
 
 // O(n) O(1) n==time, 2ms 7MB
+//int passThePillow(int n, int time) {
+//	// range from 1 to n
+//	// maintain direction variable and index variable
+//	// while time is positive, update current index with current direction
+//	// if the index reach the end of the line, multiply direction by -1.
+//
+//	int i = 1, direction = 1;
+//	while (time)
+//	{
+//		i += direction;
+//
+//		// reverse the direction if the next position is out of bounds
+//		if (i == n || i == 1)
+//			direction = -direction;
+//
+//		time--;
+//	}
+//
+//	return i;
+//}
+
+// https://leetcode.com/problems/pass-the-pillow/solutions/5424192/o-1-mod-2n-2-c-2-line-python-1-line/?envType=daily-question&envId=2024-07-06
+// O(1) O(1) 
 int passThePillow(int n, int time) {
-	// range from 1 to n
-	// maintain direction variable and index variable
-	// while time is positive, update current index with current direction
-	// if the index reach the end of the line, multiply direction by -1.
-
-	int i = 1, direction = 1;
-	while (time)
-	{
-		i += direction;
-
-		// reverse the direction if the next position is out of bounds
-		if (i == n || i == 1)
-			direction = -direction;
-
-		time--;
-	}
-
-	return i;
+	int N = 2 * n - 2;
+	int x = time % N;
+	return 1 + ((x < n) ? x : N - x);
 }
-
 
 int main() {
 	cout << boolalpha;

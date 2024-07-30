7d83a0ff7507e3eab8dff4b89cff65ccdace7834
vinhbhn <bvinhhn@gmail.com>
2024-07-30 21:51:19 +0700

    easy: 3174. clear digits

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -17,45 +17,35 @@
 
 using namespace std;
 
-// O(N) 14ms 14.2MB clever string manipulation
-//bool repeatedSubstringPattern(string s) {
-//	string ss = s + s; 
-//	string str = ss.substr(1, ss.size() - 2); // remove first and last character
-//
-//	// if str contain s meaning s have substring
-//	return str.find(s) != string::npos; 
-//}
+// O(NlogN) O(1) 0ms 8.9MB
+string clearDigits(string s) {
+	stack<char> st;
 
-// https://leetcode.com/problems/repeated-substring-pattern/solutions/3938580/99-42-2-approaches-o-n/
-// check all possible substring
-// O(n^2) O(n) 50ms 94.4MB
-bool repeatedSubstringPattern(string s) {
-	int n = s.size();
-
-	for (int i = 1; i <= n / 2; i++)
+	for (auto ch : s)
 	{
-		if (n % i == 0)
-		{
-			string sub = s.substr(0, i);
-			string repeated = "";
-			for (int j = 0; j < n / i; j++)
-			{
-				repeated += sub;
-			}
-
-			if (repeated == s)
-				return true;
-		}
+		if (isalpha(ch))
+			st.push(ch);
+		else
+			st.pop();
 	}
-	return false;
+
+	string ans = "";
+	while (!st.empty())
+	{
+		ans += st.top();
+		st.pop();
+	}
+	reverse(ans.begin(), ans.end());
+
+	return ans;
 }
 
 int main() {
 	cout << boolalpha;
 
-	cout << repeatedSubstringPattern("abab") << '\n';
-	cout << repeatedSubstringPattern("aba") << '\n';
-	cout << repeatedSubstringPattern("abcabcabcabc") << '\n';
+	cout << clearDigits("abc") << '\n';
+	cout << clearDigits("cb34") << '\n';
+	cout << clearDigits("cb34a") << '\n';
 
 	return 0;
 }
